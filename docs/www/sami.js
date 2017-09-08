272980b8724d643dd6be1c7c8edf87f5f828981c
Ken McGlothlen <mcglk@artlogix.net>
2017-09-08 02:45:28 -0700

    Okay, going to bed. Fixed FEV1/FVC to recalculate as per DD.

--- docs/www/sami.js
+++ docs/www/sami.js
@@ -17,7 +17,7 @@ document.addEventListener( "DOMContentLoaded", function(){
   // If this is the background form . . .
 
   if (bgform) {
-    var changeFunc = function(){
+    var bmiCalc = function(){
       var ht = bgform.sbph.value * (bgform.sbphu.value == "i" ? 0.0254 : 0.01),
           wt = bgform.sbpw.value * (bgform.sbpwu.value == "p" ? 0.4535 : 1),
           bmi = wt / (ht * ht);
@@ -28,18 +28,31 @@ document.addEventListener( "DOMContentLoaded", function(){
       }
       bgform.sbbmivis.value = bgform.sbbmi.value;
     };
+    var sbffrCalc = function(){
+      var sbffr;
+      if( bgform.sbfvc.value > 0.1 ) {
+        sbffr = bgform.sbfev1.value / bgform.sbfvc.value * 100.0;
+        bgform.sbffr.value = sbffr.toFixed(2);
+        bgform.sbffrvis.value = sbffr.toFixed(1) + '%';
+      } else {
+        bgform.sbffrvis.value = "";
+      }
+    };
     // Assign the same onchange to the weight box.
-    bgform.sbph.onchange = changeFunc;
-    bgform.sbpw.onchange = changeFunc;
+    bgform.sbph.onchange = bmiCalc;
+    bgform.sbpw.onchange = bmiCalc;
     // And to each of the radio buttons.
     for (var i = 0, len = bgform.sbphu.length; i < len; i++) {
-      bgform.sbphu[i].onclick = changeFunc;
+      bgform.sbphu[i].onclick = bmiCalc;
     }
     for (var i = 0, len = bgform.sbpwu.length; i < len; i++) {
-      bgform.sbpwu[i].onclick = changeFunc;
+      bgform.sbpwu[i].onclick = bmiCalc;
     }
     // Oh. Yeah. And execute it since we have the form loaded.
-    changeFunc();
+    bmiCalc();
+    bgform.sbfev1.onchange = sbffrCalc;
+    bgform.sbfvc.onchange = sbffrCalc;
+    sbffrCalc();
   }
 
 } );

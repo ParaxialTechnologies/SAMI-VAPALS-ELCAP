e43130d1f065d95de73843622818c634a5148dd6
Frederick D. S. Marshall <rick.marshall@vistaexpertise.net>
2018-01-03 02:58:49 -0800

    PPI gateway routine created 2017-11-09

---
+++ routines/SAMID.m
@@ -0,0 +1,44 @@
+SAMID ;ven/toad - dd: ppis ;2017-11-09T18:33Z
+ ;;18.0;SAMI;
+ ;
+ ; %tn contains the Mumps Number Library's mathematics APIs.
+ ; CURRENTLY UNTESTED AND IN PROGRESS
+ ;
+ quit  ; no entry from top
+ ;
+ ;
+ ;
+ ;@section 0 primary development: see routine %tnul
+ ;
+ ;
+ ;
+ ;@primary-dev: Frederick D. S. Marshall (toad)
+ ;   toad@vistaexpertise.net
+ ;@primary-dev-org: Vista Expertise Network (ven)
+ ;   http://vistaexpertise.net
+ ;@copyright: 2017, ven, all rights reserved
+ ;@license: Apache 2.0
+ ;   https://www.apache.org/licenses/LICENSE-2.0.html
+ ;
+ ;@last-updated: 2017-11-09T18:33Z
+ ;@application: Screening Applications Management - IELCAP (SAMI)
+ ;@module: Data Dictionary Support - SAMID
+ ;@version: 18.0T04
+ ;@release-date: not yet released
+ ;@patch-list: none yet
+ ;
+ ;@contents
+ ; all private programming interfaces
+ ;
+ ;
+ ;
+ ;@section 1 SAMIDV validation library procedures
+ ;
+ ;
+ ;
+ ;@PPI VAL^SAMID, validate form field against dd field
+VAL(%prec,%plimit) goto VAL^SAMIDV
+ ;
+ ;
+ ;
+EOR ; end of routine SAMID

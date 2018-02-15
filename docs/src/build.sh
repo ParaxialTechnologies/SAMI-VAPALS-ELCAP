cb7a487a5db5b3f35299cca66b3af0ab7b196f68
Domenic DiNatale <domenic@intellitechinnovations.com>
2018-02-15 12:40:37 -0500

    Added jsdoc reports as part of the build to document JS code. Added copyright headers to JS files.

--- docs/src/build.sh
+++ docs/src/build.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 pip install Jinja2
 
-python ./build.py
+python ./build.py#Check for Node.js installation
+command -v npm >/dev/null 2>&1 || {
+    >&2 echo "ERROR: Go install Node.js. It's required for generating documentation using jsdoc";
+    >&2 echo "ERROR: https://docs.npmjs.com/getting-started/installing-node";
+    exit 1;
+}
+
+#Install JSDOC if necessary
+command -v jsdoc >/dev/null 2>&1 || { npm install -g jsdoc; }
+
+# Build Code documentation
+DOCS="${DIR}/doc"
+rm -rf "${DOCS}"
+jsdoc ${DIR}/../www/*.js --readme ${DIR}/../src/README.md -d "${DOCS}"

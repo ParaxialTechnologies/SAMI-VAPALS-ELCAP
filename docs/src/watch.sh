e1849098011976e6c96394296f7d515a375d6da2
Domenic DiNatale <domenic@intellitechinnovations.com>
2018-02-15 12:49:30 -0500

    added developer utility script for watching Jinja source files; added README file for /src directory (compiled into jsdoc)

--- docs/src/watch.sh
+++ docs/src/watch.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# This is a utility script used by a developer to continuously watch and compile the Jinja templates into HTML.
+
+
 # Install the when-changed program if not available
 command -v when-changed >/dev/null 2>&1 || { sudo pip install https://github.com/joh/when-changed/archive/master.zip; }
 
 #automatically build files when changed.
-echo "Watching `pwd`..."
-when-changed -vrs . 'python ./build.py'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Watching ${DIR}..."
+when-changed -vrs "${DIR}/" "python ${DIR}/compile-forms.py"

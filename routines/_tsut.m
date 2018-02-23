30a8be152ebfd66409ba6db42497d1ff27ff58f5
Frederick D. S. Marshall <rick.marshall@vistaexpertise.net>
2018-02-22 16:35:11 -0800

    exempt cover^%tsut from code coverage

--- routines/_tsut.m
+++ routines/_tsut.m
@@ -1,4 +1,4 @@
-%tsut ;ven/mcglk&toad-type string: unit tests ;2018-02-22T23:15Z
+%tsut ;ven/mcglk&toad-type string: unit tests ;2018-02-23T00:32Z
  ;;1.8;Mash;
  ;
  ; %tsut implements unit tests for the Mash String Library.
@@ -29,7 +29,7 @@
  ;@license: Apache 2.0
  ; https://www.apache.org/licenses/LICENSE-2.0.html
  ;
- ;@last-updated: 2018-02-22T23:15Z
+ ;@last-updated: 2018-02-23T00:32Z
  ;@application: Mumps Advanced Shell (Mash)
  ;@module: Type String - %ts
  ;@version: 1.8T04
@@ -80,7 +80,7 @@ cover ; run tests & calculate code coverage
  ;
  ; to exclude specific routines, do that here:
  ;----------------------------------------------------------------------------
- set %tsuexcl(1)=""
+ set %tsuexcl(1)="cover^%tsut"
  ; set %tsuexcl(#)="EXCLUDEME^TESTROUTINE"
  ;----------------------------------------------------------------------------
  ;

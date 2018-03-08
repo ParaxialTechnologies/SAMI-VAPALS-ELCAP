afc185da7f407d2bda1e97e0eed361d966e68818
Domenic DiNatale <domenic@intellitechinnovations.com>
2018-03-08 10:16:55 -0500

    Run compiled HTML files through HTMLTidy

--- docs/src/build.sh
+++ docs/src/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-pip install Jinja2
-python "${DIR}/compile-forms.py"
+#Check for Jinja2 installation
+if [ -z `pip freeze|grep Jinja2` ]; then
+    echo "Installing Jijna2..."
+    pip install Jinja2
+fi
 
 #Check for Node.js installation
 command -v npm >/dev/null 2>&1 || {
@@ -12,8 +13,37 @@ command -v npm >/dev/null 2>&1 || {
     exit 1;
 }
 
-#Install JSDOC if necessary
+#Check for HTMLTidy installation
+command -v tidy >/dev/null 2>&1 || {
+    >&2 echo "ERROR: Please install HTMLTidy version 5.6 or greater.";
+    >&2 echo "ERROR: http://binaries.html-tidy.org/";
+    exit 1;
+}
+
+#Verify version of HTMLTidy (for HTML5)
+TIDY_VER=`tidy --version`
+if [[ $TIDY_VER != *" 5.6."* ]]; then
+   >&2 echo "ERROR: HTMLTidy detected, but is not 5.6.*: ${TIDY_VER}"
+   exit 1;
+fi
+
+#Check for JSDOC Installation
 command -v jsdoc >/dev/null 2>&1 || { npm install -g jsdoc; }
+
+
+# Compile forms
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python "${DIR}/compile-forms.py"
+
+# Pass each HTML file through HTMLTidy
+for i in ${DIR}/../www/*.html; do
+    if [[ $i = *"-form"* ]]; then
+        echo "Ignoring non-enhanced file ${i}"
+    else
+        echo "Tidying ${i}"
+        tidy -config "${DIR}/htmltidy.conf" "${i}"
+    fi
+done
 
 # Build Code documentation
 DOCS="${DIR}/doc"

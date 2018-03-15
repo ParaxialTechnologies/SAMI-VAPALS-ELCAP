9bf5eb2b73060b948b41806ad0909818d30eafe9
Domenic DiNatale <domenic@intellitechinnovations.com>
2018-03-15 10:54:55 -0400

    replaced HTMLTidy with BeautifulSoup library

--- docs/src/compile-forms.py
+++ docs/src/compile-forms.py
@@ -1,7 +1,7 @@
 import datetime
 import sys
+from bs4 import BeautifulSoup
 from collections import OrderedDict
-
 from jinja2 import Environment, FileSystemLoader
 
 reload(sys)
@@ -16,7 +16,7 @@ start = datetime.datetime.now()
 
 # background form
 with open("../www/background.html", "wb") as fh:
-    fh.write(env.get_template('background.html.jinja2').render(
+    html = env.get_template('background.html.jinja2').render(
         title='Background Form',
         occupations=OrderedDict([
             ("-", "-"),
@@ -28,33 +28,38 @@ with open("../www/background.html", "wb") as fh:
             ("6", "Machine Operators"),
             ("7", "Unskilled Employees"),
             ("8", "Unemployed")
-        ])
-    ))
+        ]))
+    fh.write(BeautifulSoup(html, 'html5lib').prettify().encode('utf-8'))
 
 with open("../www/intake.html", "wb") as fh:
-    fh.write(env.get_template('intake.html.jinja2').render(
+    html = env.get_template('intake.html.jinja2').render(
         title='Intake Form'
-    ))
+    )
+    fh.write(BeautifulSoup(html, 'html5lib').prettify().encode('utf-8'))
 
 with open("../www/ctevaluation.html", "wb") as fh:
-    fh.write(env.get_template('ctevaluation.html.jinja2').render(
+    html = env.get_template('ctevaluation.html.jinja2').render(
         title='CT Evaluation Form'
-    ))
+    )
+    fh.write(BeautifulSoup(html, 'html5lib').prettify().encode('utf-8'))
 
 with open("../www/home.html", "wb") as fh:
-    fh.write(env.get_template('home.html.jinja2').render(
+    html = env.get_template('home.html.jinja2').render(
         title="Home"
-    ))
+    )
+    fh.write(BeautifulSoup(html, 'html5lib').prettify().encode('utf-8'))
 
 with open("../www/casereview.html", "wb") as fh:
-    fh.write(env.get_template('casereview.html.jinja2').render(
+    html = env.get_template('casereview.html.jinja2').render(
         title="Case Review"
-    ))
+    )
+    fh.write(BeautifulSoup(html, 'html5lib').prettify().encode('utf-8'))
 
 with open("../www/newform.html", "wb") as fh:
-    fh.write(env.get_template('newform.html.jinja2').render(
+    html = env.get_template('newform.html.jinja2').render(
         title="New Form"
-    ))
+    )
+    fh.write(BeautifulSoup(html, 'html5lib').prettify().encode('utf-8'))
 
 elapsedMs = (datetime.datetime.now() - start).microseconds / 1000
 

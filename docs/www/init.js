ffa42b05f808f093ada8c1e766987a8119379ee8
Domenic DiNatale <domenic@intellitechinnovations.com>
2018-03-27 11:48:20 -0400

    added new functions to VAPALS javascript; split out helper functions and initialization code into a separate file.

---
+++ docs/www/init.js
@@ -0,0 +1,102 @@
+/**
+ * VA-PALS Global helper functions.
+ * @link https://github.com/OSEHRA/VA-PALS
+ * @author Domenic DiNatale <domenic.dinatale@paraxialtech.com>
+ * @license [Apache-2.0]{@link https://www.apache.org/licenses/LICENSE-2.0.html}
+ * @copyright 2018 [VA-PALS]{@link http://va-pals.org/}
+ * @class
+ */
+
+function numericHandlerKeydown(e) {
+    //console.log("k:" + e.keyCode + " s:" + e.shiftKey + " m:" + e.metaKey + " c:" + e.ctrlKey)
+    // Allow: backspace, delete, tab, escape, enter, . (decimal) (add 173 for - key)
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+        // Allow: Ctrl+A,C,V,X
+        ($.inArray(e.keyCode, [65, 67, 86, 88]) !== -1 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        // let it happen, don't do anything
+        return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+}
+
+/**
+ * Extension of form validation framework to validate a date field in DD/MMM/YYYY fommat
+ * @param value the value to validate
+ * @param validator the validator name
+ * @param $field object of the field being validated
+ * @returns {boolean} true if valid.
+ */
+function validateInternationalDate(value, validator, $field) {
+    var enabled = $field.is(":enabled");
+    console.log("validateInternationalDate() Entered. value=" + value + ", validator=" + validator + ", field=" + $field.attr("id") + ", enabled: " + enabled)
+    if (!enabled) {
+        console.log("validateInternationalDate() field is disabled. Returning true")
+        return true;
+    }
+    var m = new moment(value, VAPALS.DATE_FORMAT, true);
+    if (!m.isValid()) {
+        console.log("validateInternationalDate() field is invalid. Returning false")
+        return false;
+    }
+    console.log("validateInternationalDate() field is valid. Returning true")
+    return true;   // return true instead of value
+}
+
+/**
+ * Helper function that revalidates a date when it has changed by manual change or via the datepicker plugin
+ * @param e
+ * @returns {boolean}
+ */
+function validateDatePickerOnChange(e) {
+    var $datePicker = $(e.target)
+    var fv = $datePicker.closest(".validated").data("formValidation");
+    var $datePickerInput = $datePicker.find("input").addBack("input");
+    if (fv && $datePickerInput.is(":enabled")) {
+        fv.revalidateField($datePickerInput);
+    }
+    return true;
+}
+
+
+//global application handlers
+$(function () {
+    $("body").on('keydown', 'input.numeric', numericHandlerKeydown);
+
+    $.each($(".datepicker"), function (i, el) {
+        var $input = $(el).find("input").addBack("input"); //find the actual input control
+        var maxDateValue = $input.data('max-date');
+        var maxDate = maxDateValue ? moment(maxDateValue, "DD/MMM/YYYY").endOf('day') : false; //false == no max
+        $(el).datetimepicker({
+            format: 'DD/MMM/YYYY',
+            useCurrent: false,
+            keepInvalid: true,
+            maxDate: maxDate //hack to not select today
+        })
+            .on("dp.change", validateDatePickerOnChange);
+    });
+
+    $.each($(".yearpicker"), function (i, el) {
+        $(el).datetimepicker({
+            format: 'YYYY',
+            maxDate: moment().add(1, 'h'), //hack to not select today
+            minDate: moment().subtract(100, 'y')
+        }).on("dp.change", validateDatePickerOnChange);
+    });
+
+    //Globally enabled popovers for things like help text.
+    $('[data-toggle="popover"]').popover();
+
+    $("#save-for-later-button").on('click', function () {
+        $("input[name=status]").val("incomplete");
+    });
+
+    $("#submit-button").on('click', function () {
+        $("input[name=status]").val("complete");
+    });
+
+});
--- docs/www/vapals.js
+++ docs/www/vapals.js
@@ -80,100 +80,57 @@ var VAPALS = new function () {
         var response = Math.abs(ageDate.getUTCFullYear() - 1970);
         console.log("computeAge() returning. response=" + response);
         return response
+    };
+
+    /**
+     * Computes the number of months between the provided date and now.
+     * @param {date|moment|string} sinceDate the date
+     * @returns {number}
+     */
+    this.computeMonthsFrom = function (sinceDate) {
+        console.log("computeMonthsFrom() entered. sinceDate=" + sinceDate);
+        var since = this.toMoment(sinceDate);
+        if (since != null && since.isValid()) {
+            return this.computeMonthsBetween(since, moment());
+        }
+        return 0;
     }
 
-    this.notCovered = function () {
-        console.log("cover me!")
+    /**
+     * Computes the number of months between the provided date and now.
+     * @param {date|moment|string} from a from date
+     * @param {date|moment|string} to a to date
+     * @returns {number}
+     */
+    this.computeMonthsBetween = function (from, to) {
+        console.log("computeMonthsBetween() entered. from=" + from + ", to=" + to);
+
+        var fromMoment = this.toMoment(from);
+        var toMoment = this.toMoment(to);
+        if (fromMoment.isValid() && toMoment.isValid()) {
+            return Math.abs(parseInt(fromMoment.diff(toMoment, "months", true)))
+        }
+        return 0;
     }
+
+    /**
+     * Converts a value into a moment object
+     * @param {date|moment|string} value the value
+     * @returns {moment}
+     */
+    this.toMoment = function (value) {
+        if (moment.isMoment(value)) {
+            return value;
+        }
+        else if ($.type(value) === "date") {
+            return moment(value);
+        }
+        else {
+            return moment(value, VAPALS.DATE_FORMAT);
+        }
+    };
 
     this.DATE_FORMAT = "DD/MMM/YYYY";
 
     this.DATE_TIME_FORMAT = "DD/MMM/YYYY hh:mm:ss";
 };
-
-
-function numericHandlerKeydown(e) {
-    //console.log("k:" + e.keyCode + " s:" + e.shiftKey + " m:" + e.metaKey + " c:" + e.ctrlKey)
-    // Allow: backspace, delete, tab, escape, enter, . (decimal) (add 173 for - key)
-    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-        // Allow: Ctrl+A,C,V,X
-        ($.inArray(e.keyCode, [65, 67, 86, 88]) !== -1 && (e.ctrlKey === true || e.metaKey === true)) ||
-        // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
-        // let it happen, don't do anything
-        return;
-    }
-    // Ensure that it is a number and stop the keypress
-    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-        e.preventDefault();
-    }
-
-}
-
-function validateInternationalDate(value, validator, $field) {
-    var enabled = $field.is(":enabled");
-    console.log("validateInternationalDate() Entered. value=" + value + ", validator=" + validator + ", field=" + $field.attr("id") + ", enabled: " + enabled)
-    if (!enabled) {
-        console.log("validateInternationalDate() field is disabled. Returning true")
-        return true;
-    }
-    var m = new moment(value, VAPALS.DATE_FORMAT, true);
-    if (!m.isValid()) {
-        console.log("validateInternationalDate() field is invalid. Returning false")
-        return false;
-    }
-    console.log("validateInternationalDate() field is valid. Returning true")
-    return true;   // return true instead of value
-}
-
-function validateDatePickerOnChange(e) {
-    var $datePicker = $(e.target)
-    var fv = $datePicker.closest(".validated").data("formValidation");
-    var $datePickerInput = $datePicker.find("input").addBack("input");
-    if (fv && $datePickerInput.is(":enabled")) {
-        fv.revalidateField($datePickerInput);
-    }
-    return true;
-}
-
-// function addNumericHandler(obj) {
-//     $(obj).keydown(numericHandlerKeydown);
-// }
-
-//global application handlers
-$(function () {
-    $("body").on('keydown', 'input.numeric', numericHandlerKeydown);
-
-    $.each($(".datepicker"), function (i, el) {
-        var $input = $(el).find("input").addBack("input"); //find the actual input control
-        var maxDateValue = $input.data('max-date');
-        var maxDate = maxDateValue ? moment(maxDateValue, "DD/MMM/YYYY").endOf('day') : false; //false == no max
-        $(el).datetimepicker({
-            format: 'DD/MMM/YYYY',
-            useCurrent: false,
-            keepInvalid: true,
-            maxDate: maxDate //hack to not select today
-        })
-            .on("dp.change", validateDatePickerOnChange);
-    });
-
-    $.each($(".yearpicker"), function (i, el) {
-        $(el).datetimepicker({
-            format: 'YYYY',
-            maxDate: moment().add(1, 'h'), //hack to not select today
-            minDate: moment().subtract(100, 'y')
-        }).on("dp.change", validateDatePickerOnChange);
-    });
-
-    //Globally enabled popovers for things like help text.
-    $('[data-toggle="popover"]').popover();
-
-    $("#save-for-later-button").on('click', function(){
-        $("input[name=status]").val("incomplete");
-    });
-
-    $("#submit-button").on('click', function(){
-        $("input[name=status]").val("complete");
-    });
-
-});

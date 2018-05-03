6045cd95e851deed7e454086c39b846013858f03
Domenic DiNatale <domenic@intellitechinnovations.com>
2018-05-03 13:04:41 -0400

    removed references to legacy function synChk in enhanced forms

--- docs/src/watch.sh
+++ docs/src/watch.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 # This is a utility script used by a developer to continuously watch and compile the Jinja templates into HTML.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+# Initial setup and build
+${DIR}/build.sh
 
 # Install the when-changed program if not available
 command -v when-changed >/dev/null 2>&1 || { sudo pip install https://github.com/joh/when-changed/archive/master.zip; }
 
 #automatically build files when changed.
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Watching ${DIR}..."
 when-changed -vrs "${DIR}/" "${DIR}/build.sh"

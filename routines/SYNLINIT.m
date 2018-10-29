69566f72668107daec24b5a6c80d9b0213814c98
Larry Carlson <larry.g.carlson@gmail.com>
2018-10-29 14:37:50 -0700

    Update lab generation routine.

--- routines/SYNLINIT.m
+++ routines/SYNLINIT.m
@@ -1,12 +1,33 @@
-SYNLINIT ; ven/lgc - Initialize Lab in a new VistA instance ; 10/12/18 1:58pm
+SYNLINIT ; ven/lgc - Initialize Lab in a new VistA instance ; 10/16/18 10:50am
  ;;;1.0;;;AUG 6, 2018
  ;
  ;
  Q ; Not from top
  ;
+ ;@routine-credits
+ ;@primary development organization: Vista Expertise Network
+ ;@primary-dev: Larry G. Carlson (lgc)
+ ;@copyright:
+ ;@license: Apache 2.0
+ ; https://www.apache.org/licenses/LICENSE-2.0.html
  ;
-EN ; Get institution
- N IEN4
+ ;@last-updated: 2018-09-26
+ ;@application: VA-PALS
+ ;@version: 1.0
+ ;
+ ;@sac-exemption
+ ; sac 2.2.8 Vendor specific subroutines may not be called directly
+ ;  except by Kernel, Mailman, & VA Fileman.
+ ; sac 2.3.3.2 No VistA package may use the following intrinsic
+ ;  (system) variables unless they are accessed using Kernel or VA
+ ;  FileMan supported references: $D[EVICE], $I[O], $K[EY],
+ ;  $P[RINCIPAL], $ROLES, $ST[ACK], $SY[STEM], $Z*.
+ ;  (Exemptions: Kernel & VA Fileman)
+ ; sac 2.9.1: Application software must use documented Kernel
+ ;  supported references to perform all platform specific functions.
+ ;  (Exemptions: Kernel & VA Fileman)
+ ;
+EN N IEN4
  S IEN4=$$GETINST
  I '$G(IEN4) D  Q
  . W !,!,"*** You must select an institution to continue.",!,!
@@ -17,12 +38,6 @@ EN ; Get institution
  ; Make sure each accession area has an Identity
  D SetAccessionIDs
  ;
- ; Read csv file and build LOINCA array
- ;D READCSV(fileloc)
- ;
- ; Build the LOINCA array
- ;N LOINCA S LOINCA="" D BLDGS(.LOINCA)
- ;
  ; Build ^KBAP("LABARR" global
  D BLDKBAP
  ;
@@ -30,14 +45,22 @@ EN ; Get institution
  ; Run through TMPARR and update test
  ;   for accession area, institution
  ;   for site/specimen, LOINC, UNITS
- D UPDTLAB
+ ;D UPDTLAB
+ ;
  ; Make sure rollover running
  D SchedRollover
+ ;
  ; Run rollover manually now
  W !,!," *** Running Lab Rollover manually now.",!
  D ^LROLOVER
  Q
  ;
+ ;
+ ; Enter
+ ;   nothing required
+ ; Return
+ ;   0  = failure to select an institution
+ ;   >0 = ien of selected entry in INSTITUTION [#4] file
 GETINST() ;
  N DIC,CNT,IEN60,NAME,INST,LRAC
  S DIC("A")="Select an institution to assign to the lab tests : "
@@ -53,203 +76,79 @@ GETINST() ;
  ;
  ;
  ;
- ; Note:
- ; Format of data in Loinc.cvs from loinc.org - SEE BELOW
- ;
- ; Read Loinc.csv
- ; Enter
- ;    FILELOC  = location of file on system
- ; Return
- ;    LOINCA array - piece - description
- ;                   1       LOINC   [14106-9]
- ;                   2       Test name [Lymphocytes]
- ;                   3       Property [NCnc]
- ;                   4       Site/Specimen [CSF]
- ;                   5       Accession Area [HEM/BC]
- ;                   6       Short name
- ;                   7       Is Active [ACTIVE]
- ;                   8       Example units [10*3/uL]
- ;                   9       Description
-READCSV(FILELOC) ;
- I $G(FILELOC)="" W !,"Call with file location",!,!  Q
- ;D OPEN^ZISHGTM(1,"/home/osehra/tmp/","Loinc.csv","R")
- D OPEN^ZISHGTM(1,FILELOC,"Loinc.csv","R")
- N ISACT,STR,LIMIT,CNT,CMPNT,PRPRTY,SYSTM,CLASS
- N SHRTNM
- S (CNT,LIMIT)=0,STR=""
- K LOINCA
- U IO
- F  R X#1 D  Q:LIMIT>87800
- . S STR=STR_X
- . I X[$C(13) D
- .. S STR=$TR(STR,"""")
- .. S CNT=CNT+1
- .. S LOINC=$P(STR,",",1)
- .. S CMPNT=$P(STR,",",2)
- .. S PRPRTY=$P(STR,",",3)
- .. S SYSTM=$P(STR,",",5)
- .. S CLASS=$P(STR,",",8)
- .. S DESC=$P(STR,",",11)
- .. S ISACT=$P(STR,",",12)
- .. S EXMPUNTS=$P(STR,",",28)
- .. S STR=""
- .. S SHRTNM=$P(STR,",",23)
- .. I ISACT["ACTIVE" D
- ... S LOINCA(CNT)=LOINC_"^"_CMPNT_"^"_PRPRTY_"^"_SYSTM_"^"_CLASS_"^"_SHRTNM_"^"_ISACT_"^"_EXMPUNTS_"^"_DESC_"^"
- .. S LIMIT=LIMIT+1
- ;
- D CLOSE^ZISHGTM(1)
- Q
- ;
- ;
- ; Use the LOINCA array to build the
- ;   "loinc org data" Graphstore
- ; *** WARNING *** over 87,000 nodes may be
- ;   added to journal with purge of existing file
-BLDGS(LOINCA) ;
- n root,gien,NODE,SNODE,STR,name,loinc
- d purgegraph^%wd("loinc org data")
- s root=$$setroot^%wd("loinc org data")
- s gien=0
- S NODE=$NA(LOINCA),SNODE=$P(NODE,")")
- F  S NODE=$Q(@NODE) Q:NODE'[SNODE  D
- . S STR=@NODE
- . s loinc=$P(STR,"^")
- . s name=$P(STR,"^",2)
- . s gien=gien+1
- . S @root@(gien,"loinc")=$P(STR,"^")
- . S @root@(gien,"test name")=name
- . S @root@(gien,"property")=$P(STR,"^",3)
- . S @root@(gien,"site/specimen")=$P(STR,"^",4)
- . S @root@(gien,"accession area")=$P(STR,"^",5)
- . S @root@(gien,"example units")=$P(STR,"^",8)
- . S @root@(gien,"description")=$P(STR,"^",9)
- . S:'(name="") @root@("name",$$UP^XLFSTR(name),gien)=""
- . S:'(loinc="") @root@("loinc",loinc,gien)=""
- ;
- Q
- ;
- ;
- ; Build ^KBAP("LABARR" file
- ;  e.g.
- ; KBAP("LABARR",143,"lnc","UA","Urine",5811,"5811-5","SPECIFIC GRAVITY",47921)
- ; ^KBAP("LABARR",143,"lod","CHEM","Amnio fld",14345,"14345-3","SPECIFIC GRAVITY,4215)
- ; ...
- ; ^KBAP("LABARR",143,"lod","CHEM","Urine",2965,"2965-2","SPECIFIC GRAVITY",19256)
- ; ...
- ;
  ; Build a global ^KBAP("LABARR") by running through all
  ;  names and synonyms in file 60 and finding entries
- ;  in the "loinc-code-map" and "loinc org data"
- ;  Graphstores.  The ^KBAP("LABARR") global should
+ ;  in the LAB LOINC [#95.3] that relate each test.
+ ;  The ^KBAP("LABARR") global should
  ;  be helpful in automating the updating of the
  ;  test entries in file 60 with accession area,
  ;  site/specimen, loinc, and, perhaps, unit information
- ; ^KBAP("LABARR",x1,x2,x3,x4,x5,x6,x7,x8)=units
- ;    subscript   Description
- ;    x1          IEN of test in file 60
- ;    x2          'inc' info from 'loinc-code-map'
- ;                'lod' info from 'loinc org data'
- ;                '95.3' info from LAB LOINC file
- ;                NOTE: to give preference to loinc-code-map data
- ;    x3          Accession Area
- ;    x4          Site/Specimen
- ;    x5          LOINC integer
- ;    x6          LOINC with checksum digit
- ;    x7          Test n
- ;    x8          'loinc org data' gien
-BLDKBAP ;
- N rootlcm,rootlod,lcmtest,lodtest,lcmien,F60IEN
- N CNT,lcmlnc,lcmgien,lodgien,EXMPUNTS
- N F60NAME,LCMGIEN,LCMLNC,LODGIEN,ACCAREA,SITESP,F60SYN
- N PRPRTY
- s rootlod=$$setroot^%wd("loinc org data")
- s rootlcm=$$setroot^%wd("loinc-code-map")
+ ; Structure
+ ;   Subscript    Comment
+ ;   1            "LABARR"
+ ;   2            IEN into LABORATORY TEST [#60] file
+ ;   3            "95.3" LAB LOINC file indicating source of info
+ ;   4            Accession Area for test
+ ;   5            Site/Specimen for test
+ ;   6            LOINC integer
+ ;   7            Full LOINC with checksum digit
+ ;   8            Test name
+ ;   9            UNITS for test result
  ;
- ; 1) run down each entry in file 60 pulling each test name
- ;    then look up this name in in 'loinc-code-map'
- ; 2) run down each entry in file 60 SYNONYMS on each test
- ;    then look up this name in 'loinc-code-map'
- ; With each step, build the ^KBAP("LABARR" global for use
- ;   later in building/editing lab tests in file 60
- ;   [UPDTLAB]
- ;
- K ^KBAP("LABARR")
+ ;  e.g.
+ ;^KBAP("LABARR",1,95.3,"HEM/BC","Body fluid, unsp",6743,"6743-9","WBC","10*6/L")=""
+ ; Enter
+ ;   nothing required
+ ; Return
+ ;   ^KBAP("LABARR" global built
+BLDKBAP K ^KBAP("LABARR");
  S F60IEN=0
 BNAME F  S F60IEN=$O(^LAB(60,F60IEN)) Q:'F60IEN  D
  . S F60NAME=$$UP^XLFSTR($P($G(^LAB(60,F60IEN,0)),"^"))
- .; D GETLCM(F60IEN,F60NAME)
- .; D GETLOD(F60IEN,F60NAME)
+ . I $L(F60NAME)<3 W !,"NAME :",F60IEN," ",F60NAME
  . D GET953(F60IEN,F60NAME)
  S F60IEN=0
 BSYN F  S F60IEN=$O(^LAB(60,F60IEN)) Q:'F60IEN  D
  . S F60SYN=""
  . F  S F60SYN=$O(^LAB(60,F60IEN,5,"B",F60SYN)) Q:($TR(F60SYN," ")="")  D
- ..; D GETLCM(F60IEN,F60SYN)
- ..; D GETLOD(F60IEN,F60SYN)
+ .. I $L(F60SYN)<3 W !,"SYNONYM: ",F60IEN," ",F60SYN," ",$$GET1^DIQ(60,F60IEN,.01)
  .. D GET953(F60IEN,F60SYN)
  Q
  ;
-GETLCM(F60IEN,F60NAME) ; Data from "loinc-code-map"
- S LCMGIEN=$O(@rootlcm@("graph","map","pos","NAME",F60NAME,0))
- Q:'LCMGIEN
- S LCMLNC=$G(@rootlcm@("graph","map",LCMGIEN,"LOINC"))
- Q:'$L(LCMLNC)
- ; Data from "loinc org data" for this loinc entry
- S LODGIEN=$O(@rootlod@("loinc",LCMLNC,0))
- Q:'LODGIEN
- S ACCAREA=$G(@rootlod@(LODGIEN,"accession area"))
- S SITESP=$G(@rootlod@(LODGIEN,"site/specimen"))
- S EXMPUNTS=$G(@rootlod@(LODGIEN,"example units"))
- S PRPRTY=$G(@rootlod@(LODGIEN,"property"))
- S:PRPRTY="" PRPRTY="UNK"
- Q:'$L(ACCAREA)  Q:'$L(SITESP)
- S ^KBAP("LABARR",F60IEN,"lnc",ACCAREA,SITESP,$P(LCMLNC,"-"),LCMLNC,F60NAME,LODGIEN,PRPRTY)=EXMPUNTS
- Q
- ;
- ;
-GETLOD(F60IEN,F60NAME) ; Data from "loinc org data"
- S LODGIEN=0
- F  S LODGIEN=$O(@rootlod@("name",F60NAME,LODGIEN)) Q:'LODGIEN  D
- . S ACCAREA=$G(@rootlod@(LODGIEN,"accession area"))
- . S SITESP=$G(@rootlod@(LODGIEN,"site/specimen"))
- . S EXMPUNTS=$G(@rootlod@(LODGIEN,"example units"))
- . S PRPRTY=$G(@rootlod@(LODGIEN,"property"))
- . S:PRPRTY="" PRPRTY="UNK"
- . S LODLNC=$G(@rootlod@(LODGIEN,"loinc"))
- . Q:'$L(ACCAREA)  Q:'$L(SITESP)  Q:'$L(LODLNC)
- .; Limit entries to those with certain accession areas
- . Q:"^CHEM^SERO^UA^DRUG/TOX^MICRO^HEM/BC"'[ACCAREA
- . S ^KBAP("LABARR",F60IEN,"lod ",ACCAREA,SITESP,$P(LODLNC,"-"),LODLNC,F60NAME,LODGIEN,PRPRTY)=EXMPUNTS
- Q
  ;
  ; Find entries in 95.3 that match for this test in file 60
  ;Entry
  ;   F60IEN   = IEN of lab test in file 60
  ;   F60NAME  = name of lab test in file 60
  ;Return
- ;   ^KBAP("LABARR",F60IEN,"95.3",ACCAREA,SITESP,$P(LODLNC,"-"),LODLNC,F60NAME,LODGIEN,PRPRTY)=EXMPUNTS
- ;
+ ;   Update ^KBAP("LABARR" global with info on this test
 GET953(F60IEN,F60NAME) ; Data from LAB LOINC [95.3]
  ; Find potential matches in 95.3
- N SF60NAME,SF60L,DONE
- S DONE=0
- S SF60NAME=$E(F60NAME,1,$L(F60NAME)-1)
- S SF60L=$L(SF60NAME)
- S NODE=$NA(^LAB(95.3,"E",SF60NAME)),SNODE=$P($NA(^LAB(95.3,"E")),")")
- F  S NODE=$Q(@NODE) Q:NODE'[SNODE  D  Q:DONE
- . I '($E($P($TR(NODE,""""),",",3),1,SF60L)[SF60NAME) D  Q
- .. S DONE=1
+ N SearchName,SearchNameLength
+ ; Back off 1 character in the name of the entry in
+ ;   file 60 so the next $Q node won't pass by the full name
+ ;   Save the shortened search name and the length of the
+ ;   shortened name
+ S SearchName=F60NAME,SearchNameLength=$L(SearchName)
+ ;
+ ; Use the LAB LOINC file "E" cross reference to pair with
+ ;  test entries in file 60
+ S NODE=$NA(^LAB(95.3,"E",SearchName))
+ ; We are done with further searching in the E cross reference
+ ;   when we have passed our search string
+ F  S NODE=$Q(@NODE) Q:'($E($QS(NODE,3),1,SearchNameLength)[SearchName)  D
+ . W !,NODE
  . N LabLoincIEN,LabLoinc0node,LabAccArea64061ien,LabAccessionArea
  . N LabLoincFull,LabLoincShortName,LabLoincCmptIEN
- . N LabLoincCmptName,LabElectronicCodesIEN,SYSTEM
- . N Units
+ . N LabLoincComponentName,LabElectronicCodesIEN,SYSTEM
+ . N Units,LabSiteSpecimen
  . S LabLoincIEN=$QS(NODE,4)
  . Q:'$G(LabLoincIEN)
  .; Quit if CLASSTYPE is not LABORATORY
  . Q:($G(^LAB(95.3,LabLoincIEN,3))>2)
- .; Quit if STATUS is not DEL, TRIAL, or DISCOURAGED
+ .; Quit if STATUS is DEL, TRIAL, or DISCOURAGED
  . Q:'($P($G(^LAB(95.3,LabLoincIEN,4)),"^")="")
+ . S LoincFullySpecifiedName=$G(^LAB(95.3,LabLoincIEN,80))
  . S LabLoinc0node=$G(^LAB(95.3,LabLoincIEN,0))
  . S LabAccArea64061ien=$P(LabLoinc0node,"^",11)
  . S LabAccessionArea=$P($G(^LAB(64.061,LabAccArea64061ien,0)),"^")
@@ -257,17 +156,16 @@ GET953(F60IEN,F60NAME) ; Data from LAB LOINC [95.3]
  . S LabLoincShortName=$G(^LAB(95.3,LabLoincIEN,81))
  .; Get Lab LOINC Component [95.31] entry
  . S LabLoincCmptIEN=$P(LabLoinc0node,"^",2)
- . S LabLoincCmptName=$G(^LAB(95.31,LabLoincCmptIEN,0))
- .; Get SYSTEM or entry into LAB ELECTRONICS CODES [64.061] file
+ . S LabLoincComponentName=$G(^LAB(95.31,LabLoincCmptIEN,0))
  . S LabElectronicCodesIEN=$P(LabLoinc0node,"^",8)
  . Q:'$G(LabElectronicCodesIEN)
- . S SYSTEM=$G(^LAB(64.061,LabElectronicCodesIEN,0))
+ . S LabSiteSpecimen=$G(^LAB(64.061,LabElectronicCodesIEN,0))
  . S LabElectronicCodesIEN=$P(LabLoinc0node,"^",14)
  . S Units="unk"
  . S:($G(LabElectronicCodesIEN)>0) Units=$P($G(^LAB(64.061,LabElectronicCodesIEN,0)),"^")
  .;
- . I ((LabLoincCmptName=F60NAME)!($E(LabLoincShortName,1,$L(F60NAME))=F60NAME)) D
- .. S ^KBAP("LABARR",F60IEN,"95.3",LabAccessionArea,$P(SYSTEM,"^"),LabLoincIEN,LabLoincFull,F60NAME,LabLoincIEN)=Units
+ . I ((LabLoincComponentName=SearchName)!($E(LabLoincShortName,1,SearchNameLength)=SearchName)) D
+ .. S ^KBAP("LABARR",F60IEN,"95.3",LabAccessionArea,LabSiteSpecimen,LabLoincIEN,LabLoincFull,F60NAME,LabLoincIEN,Units)=LoincFullySpecifiedName
  Q
  ;
 UPDTLAB ;
@@ -302,12 +200,12 @@ UPDTTST(TMPARR) ;
  . S LOINC=$QS(NODE,5)
  . S TSTNAME=$QS(NODE,7)
  . S ACCAREA=$QS(NODE,3)
- . S PRPRTY=$QS(NODE,9)
- . S UNITS=@NODE
+ . S UNITS=$QS(NODE,9)
  . S IEN68=$$FDNAANMB(ACCAREA)
  . S SPECIMEN=$QS(NODE,4) ; e.g. Synv fld; Ser/Plas
- .; Remember SPECIMEN may represent a multiple
- .;  such as 70;71
+ .; Translate SPECIMEN string to entries in TOPOGRAPHY [#61]
+ .;  file.  Specimen strings with multiple entries (SERUM/PLASMA)
+ .;  will translate into a comma delimited
  . S SiteSpecimens=$$FNDSSNBR(SPECIMEN)
  .;
  .; If we have an accession area and one or more site/specimens
@@ -360,6 +258,7 @@ ACCAREA ;;
  ;;COAG^3
  ;;DRUG/TOX^20
  ;;HEM/BC^10
+ ;;FERT^10
  ;;MICRO^12
  ;;SERO^17
  ;;RIA^6
@@ -607,6 +506,15 @@ DispTestWithLOINC ;
  .. W !," ----- ",NODE,"=",@NODE
  .. S TOT953=$G(TOT953)+1
  W !,"Total site/specimen entries with LOINC :",TOT953
+ Q
+ ;
+GetProperty ;
+ N NODE,SNODE,PRPTY
+ K POO
+ S NODE=$NA(^KBAP("LABARR")),SNODE=$P(NODE,")")
+ F  S NODE=$Q(@NODE) Q:NODE'[SNODE  D
+ . S PRPTY=$P(@NODE,":",4)
+ . S:'(PRPTY="") POO(PRPTY)=$G(POO(PRPTY))+1
  Q
  ;
 EOR ;End of routine SYNLINIT

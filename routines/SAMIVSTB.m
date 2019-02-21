fb126e8eb269dafb87a800508b10234a11f00bdc
Linda Yaw <linda.yaw@vistaexpertise.net>
2019-02-20 18:30:30 -0800

    Spelled out commands

--- routines/SAMIVSTB.m
+++ routines/SAMIVSTB.m
@@ -1,19 +1,18 @@
-SAMIVSTB ;;ven/lgc - M2M Broker to for VA-PALS ; 1/22/19 1:41pm
+SAMIVSTB ;;ven/lgc - M2M Broker to for VA-PALS ; 2019-02-21T01:16
  ;;18.0;SAMI;;
  ;
  ;@license: see routine SAMIUL
  ;
- ; API's called by SAMIVSA only
+ ; API's called by SAMIVSTA only
  ;
  ;@routine-credits
  ;@primary development organization: Vista Expertise Network
  ;@primary-dev: Larry G. Carlson (lgc)
  ;@primary-dev: Alexis R. Carlson (arc)
  ;@copyright:
- ;@license: Apache 2.0
- ; https://www.apache.org/licenses/LICENSE-2.0.html
+ ;@license: see routine SAMIUL
  ;
- ;@last-updated: 2018-09-26
+ ;@last-updated: 2019-02-21
  ;@application: VA-PALS
  ;@version: 1.0
  ;
@@ -29,7 +28,7 @@ SAMIVSTB ;;ven/lgc - M2M Broker to for VA-PALS ; 1/22/19 1:41pm
  ;  supported references to perform all platform specific functions.
  ;  (Exemptions: Kernel & VA Fileman)
  ;
- Q  ; not from top
+ quit  ; not from top
  ;
  ;
  ;@API-code: $$VIT^SAMIVSTA
@@ -49,52 +48,52 @@ SAMIVSTB ;;ven/lgc - M2M Broker to for VA-PALS ; 1/22/19 1:41pm
  ;      1   = lookup of patient successful
  ;      2   = and update of Graphstore successful
 VIT(dfn,sdate,edate) ;
- i '$g(dfn) q:$Q 0  q
- n cntxt,rmprc,console,cntnopen,SAMIUARR,SAMIUXD
- s cntxt="ORRCMC DASHBOARD"
- s rmprc="ORRC VITALS BY PATIENT"
- s (console,cntnopen)=0
+ if '$get(dfn) quit:$Q 0  quit
+ new cntxt,rmprc,console,cntnopen,SAMIUARR,SAMIUXD
+ set cntxt="ORRCMC DASHBOARD"
+ set rmprc="ORRC VITALS BY PATIENT"
+ set (console,cntnopen)=0
  ; if no start date specified, set to 19010101
- s:+$g(sdate)=0 sdate="19000101"
+ set:+$get(sdate)=0 sdate="19000101"
  ; if no end date specified, set to now
- s:+$g(edate)=0 edate=$p($$FMTHL7^XLFDT($$HTFM^XLFDT($H)),"-")
- s SAMIUARR(1)=dfn
- s SAMIUARR(2)=sdate
- s SAMIUARR(3)=edate
- s SAMIUARR(4)="1"
- k SAMIUXD
- d M2M^SAMIM2M(.SAMIUXD,cntxt,rmprc,console,cntnopen,.SAMIUARR)
- i '$d(SAMIUXD) q:$Q 0  q
+ set:+$get(edate)=0 edate=$piece($$FMTHL7^XLFDT($$HTFM^XLFDT($horolog)),"-")
+ set SAMIUARR(1)=dfn
+ set SAMIUARR(2)=sdate
+ set SAMIUARR(3)=edate
+ set SAMIUARR(4)="1"
+ kill SAMIUXD
+ do M2M^SAMIM2M(.SAMIUXD,cntxt,rmprc,console,cntnopen,.SAMIUARR)
+ if '$data(SAMIUXD) quit:$Q 0  quit
  ;
- n ptdfn,node,gien,ptwt,ptht,ptbp,str,J,vitdt,pttemp
- s (ptdfn,ptwt,ptht,ptbp,vitdt)=0
- f J=1:1:$L(SAMIUXD,$c(13,10)) d
- . s str=$p(SAMIUXD,$c(13,10),J)
- . i str["Item=VIT:" d
- .. s ptdfn=+$p(str,":",2),vitdt=+$p(str,"^",3)
- . i str["Data=B/P",'$g(ptbp) d
- .. s ptbp=$p(str,"^",2)_"^"_vitdt
- . i str["Data=Ht.",'$g(ptht) d
- .. s ptht=$p(str,"Data=Ht.",2)
- .. s ptht=$p(ptht,"^",2,5)_"^"_vitdt
- . i str["Data=Wt.",'$g(ptwt) d
- .. s ptwt=$p(str,"Data=Wt.",2)
- .. s ptwt=$p(str,"^",2,5)_"^"_vitdt
- . i str["Data=Temp" d
- .. s pttemp=$p(str,"Data=Temp.",2)
- .. s pttemp=$p(str,"^",2,5)_"^"_vitdt
- i '$g(ptdfn),'(ptdfn=dfn) q:$Q 0  q
- n root s root=$$setroot^%wd("patient-lookup")
- s node=$na(@root@("dfn",ptdfn)),node=$q(@node)
- i '($p(node,",",4)[ptdfn) q:$Q 1  q
- s gien=+$p(node,",",5)
- i 'gien q:$Q 1  q
- s:$g(ptbp) @root@(gien,"vitals bp")=ptbp
- s:$g(ptht) @root@(gien,"vitals ht")=ptht
- s:$g(ptwt) @root@(gien,"vitals wt")=ptwt
- s:$g(pttemp) @root@(gien,"vitals temp")=pttemp
- s node=$na(@root@("dfn",ptdfn))
- q:$Q 2  q
+ new ptdfn,node,gien,ptwt,ptht,ptbp,str,J,vitdt,pttemp
+ set (ptdfn,ptwt,ptht,ptbp,vitdt)=0
+ for J=1:1:$length(SAMIUXD,$char(13,10)) do
+ . set str=$piece(SAMIUXD,$char(13,10),J)
+ . if str["Item=VIT:" do
+ .. set ptdfn=+$piece(str,":",2),vitdt=+$piece(str,"^",3)
+ . if str["Data=B/P",'$get(ptbp) do
+ .. set ptbp=$piece(str,"^",2)_"^"_vitdt
+ . if str["Data=Ht.",'$get(ptht) do
+ .. set ptht=$piece(str,"Data=Ht.",2)
+ .. set ptht=$piece(ptht,"^",2,5)_"^"_vitdt
+ . if str["Data=Wt.",'$get(ptwt) do
+ .. set ptwt=$piece(str,"Data=Wt.",2)
+ .. set ptwt=$piece(str,"^",2,5)_"^"_vitdt
+ . if str["Data=Temp" do
+ .. set pttemp=$piece(str,"Data=Temp.",2)
+ .. set pttemp=$piece(str,"^",2,5)_"^"_vitdt
+ if '$get(ptdfn),'(ptdfn=dfn) quit:$Q 0  quit
+ new root set root=$$setroot^%wd("patient-lookup")
+ set node=$name(@root@("dfn",ptdfn)),node=$query(@node)
+ if '($piece(node,",",4)[ptdfn) quit:$Q 1  quit
+ set gien=+$piece(node,",",5)
+ if 'gien quit:$Q 1  quit
+ set:$get(ptbp) @root@(gien,"vitals bp")=ptbp
+ set:$get(ptht) @root@(gien,"vitals ht")=ptht
+ set:$get(ptwt) @root@(gien,"vitals wt")=ptwt
+ set:$get(pttemp) @root@(gien,"vitals temp")=pttemp
+ set node=$name(@root@("dfn",ptdfn))
+ quit:$Q 2  quit
  ;
  ;
  ;
@@ -114,69 +113,69 @@ VIT(dfn,sdate,edate) ;
  ;      1      = lookup of patient successful
  ;      gien   = and update of Graphstore successful
 VPR(dfn) ;
- I '$g(dfn) q:$Q 0  q
- n cntxt,rmprc,console,cntnopen,SAMIUARR,SAMIUXD,root
- s cntxt="VPR APPLICATION PROXY"
- s rmprc="VPR GET PATIENT DATA"
- s (console,cntnopen)=0
- k SAMIUXD
- s SAMIUARR(1)=dfn
- s SAMIUARR(2)="demographics;reactions;problems;vitals;labs;meds;immunizations;observations;visits;appointments;documents;procedures;consults;flags;factors;skinTests;exams,education,insurance"
- d M2M^SAMIM2M(.SAMIUXD,cntxt,rmprc,console,cntnopen,.SAMIUARR)
- ;F J=1:1:$L(SAMIUXD,$C(13,10)) W !,$p(SAMIUXD,$C(13,10),J)
+ if '$get(dfn) quit:$Q 0  quit
+ new cntxt,rmprc,console,cntnopen,SAMIUARR,SAMIUXD,root
+ set cntxt="VPR APPLICATION PROXY"
+ set rmprc="VPR GET PATIENT DATA"
+ set (console,cntnopen)=0
+ kill SAMIUXD
+ set SAMIUARR(1)=dfn
+ set SAMIUARR(2)="demographics;reactions;problems;vitals;labs;meds;immunizations;observations;visits;appointments;documents;procedures;consults;flags;factors;skinTests;exams,education,insurance"
+ do M2M^SAMIM2M(.SAMIUXD,cntxt,rmprc,console,cntnopen,.SAMIUARR)
+ ;for J=1:1:$length(SAMIUXD,$char(13,10)) write !,$piece(SAMIUXD,$char(13,10),J)
  ;
- n admtdt,attnd,strng,J,node
- n inpt,pticn,ptloc,ptsrvc,ptssn,ptwt,ptht,spclty
+ new admtdt,attnd,strng,J,node
+ new inpt,pticn,ptloc,ptsrvc,ptssn,ptwt,ptht,spclty
  ;
- f J=1:1:$L(SAMIUXD,$C(13,10)) d
- . s strng=$p(SAMIUXD,$C(13,10),J)
+ for J=1:1:$length(SAMIUXD,$char(13,10)) do
+ . set strng=$piece(SAMIUXD,$char(13,10),J)
  .; Is this attending information?
- . i strng["attending code" d  q
- .. s attnd=$p(strng,"'",2)
- .. s attnd=attnd_"^"_$p(strng,"'",5)
+ . if strng["attending code" do  quit
+ .. set attnd=$piece(strng,"'",2)
+ .. set attnd=attnd_"^"_$piece(strng,"'",5)
  .;
  .; Is this the patient's SSN?
- . i strng["ssn value" d  q
- .. s ptssn=$p(strng,"'",2)
+ . if strng["ssn value" do  quit
+ .. set ptssn=$piece(strng,"'",2)
  .;
  .; Is this patient's admission data?
- . i strng["admitted id" d  q
- .. s admtdt="^DGPM("_+$p(strng,"'",3)_",^"_+$p(strng,"'",5)
+ . if strng["admitted id" do  quit
+ .. set admtdt="^DGPM("_+$piece(strng,"'",3)_",^"_+$piece(strng,"'",5)
  .;
  .; Is this patient's specialty data
- . i strng["specialty code=" d  q
- .. s spclty="CODE="_+$p(strng,"'",3)_"^"_$p(strng,"'",5)
+ . if strng["specialty code=" do  quit
+ .. set spclty="CODE="_+$piece(strng,"'",3)_"^"_$piece(strng,"'",5)
  .;
  .; Is this patient location data?
- . i strng["location code=" d  q
- .. s ptloc=$p(strng,"'",2)_"^"_$p(strng,"'",4)
+ . if strng["location code=" do  quit
+ .. set ptloc=$piece(strng,"'",2)_"^"_$piece(strng,"'",4)
  .;
  .; Is this patient's assigned service
- . i strng["locSvc code" d  q
- .. s ptsrvc=$p(strng,"'",2)
+ . if strng["locSvc code" do  quit
+ .. set ptsrvc=$piece(strng,"'",2)
  .;
  .; Is this the patient's ICN?
- . i strng["icn value=" d  q
- .. s pticn=$p(strng,"'",2)
+ . if strng["icn value=" do  quit
+ .. set pticn=$piece(strng,"'",2)
  .;
  .; Is this an inpatient
- . i strng["inpatient value" d  q
- .. s inpt=$p(strng,"'",2)
+ . if strng["inpatient value" do  quit
+ .. set inpt=$piece(strng,"'",2)
  ;
  ; Now save data in "patient-lookup" Graph Store
  ;
-VPR1 s root=$$setroot^%wd("patient-lookup")
- s node=$na(@root@("dfn",dfn)),node=$q(@node)
- i '($p(node,",",4)[dfn) q:$Q 1  q
- S gien=+$p(node,",",5)
- I 'gien q:$Q 1  q
- s @root@(gien,"attending")=$g(attnd)
- s @root@(gien,"admission")=$g(admtdt)
- s @root@(gien,"specialty")=$g(spclty)
- s @root@(gien,"location")=$g(ptloc)
- s @root@(gien,"service")=$g(ptsrvc)
- s @root@(gien,"icn")=$g(pticn)
- s @root@(gien,"inpatient")=$g(inpt)
- q:$Q $g(gien)  q
+VPR1 set root=$$setroot^%wd("patient-lookup")
+ set node=$name(@root@("dfn",dfn)),node=$q(@node)
+ if '($piece(node,",",4)[dfn) quit:$Q 1  quit
+ set gien=+$piece(node,",",5)
+ if 'gien quit:$Q 1  quit
+ set @root@(gien,"attending")=$get(attnd)
+ set @root@(gien,"admission")=$get(admtdt)
+ set @root@(gien,"specialty")=$get(spclty)
+ set @root@(gien,"location")=$get(ptloc)
+ set @root@(gien,"service")=$get(ptsrvc)
+ set @root@(gien,"icn")=$get(pticn)
+ set @root@(gien,"inpatient")=$get(inpt)
+ quit:$Q $get(gien)  quit
  ;
 EOR ; End of routine SAMIVSTB

a659b97bc01b7c097d61994b76fa9708660429b0
Larry Carlson <larry.g.carlson@gmail.com>
2019-03-19 13:42:49 -0700

    minor edits for XINDEX errors

--- routines/SAMIUL.m
+++ routines/SAMIUL.m
@@ -1,5 +1,5 @@
-SAMIUL ;ven - SAMI LICENSE ; 1/22/19 2:13pm
- ;;SAMI;;
+SAMIUL ;ven - SAMI LICENSE ; 3/19/19 1:31pm
+ ;;18.0;SAMI;;
  ;
  ;Copyright 2019 Early Diagnosis and Treatment Research Foundation, Vista Expertise Network (VEN), and Paraxial
  ;
@@ -7,6 +7,7 @@ SAMIUL ;ven - SAMI LICENSE ; 1/22/19 2:13pm
  ;
  ;Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
  ;
- ;Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.http://www.apache.org/licenses/LICENSE-2.0http://www.apache.org
+ ;Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ; See the License for the specific language governing permissions and limitations under the License.http://www.apache.org/licenses/LICENSE-2.0http://www.apache.org
  ;
 EOF ; End of routine SAMIUL

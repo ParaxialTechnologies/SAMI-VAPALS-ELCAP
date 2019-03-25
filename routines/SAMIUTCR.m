71141b36b8db5f0357adbbd4a03abd309acddd5a
Linda Yaw <linda.yaw@vistaexpertise.net>
2019-03-25 11:54:39 -0700

    Spelled out Mumps elements

--- routines/SAMIUTCR.m
+++ routines/SAMIUTCR.m
@@ -1,4 +1,4 @@
-SAMIUTCR ;ven/lgc - UNIT TEST for SAMICTR ; 3/15/19 11:15am
+SAMIUTCR ;ven/lgc - UNIT TEST for SAMICTR ; 2019-03-25T18:31Z
  ;;18.0;SAMI;;
  ;
  ;@license: see routine SAMIUL
@@ -8,13 +8,15 @@ SAMIUTCR ;ven/lgc - UNIT TEST for SAMICTR ; 3/15/19 11:15am
  ; @routine-credits
  ; @primary-dev: Larry Carlson (lgc)
  ;  larry@fiscientific.com
+ ; @additional-dev: Linda M. R. Yaw (lmry)
+ ;  linda.yaw@vistaexpertise.net
  ; @primary-dev-org: Vista Expertise Network (ven)
  ;  http://vistaexpertise.net
  ; @copyright: 2012/2018, ven, all rights reserved
  ; @license: Apache 2.0
  ;  https://www.apache.org/licenses/LICENSE-2.0.html
  ;
- ; @last-updated: 10/31/18 5:51pm
+ ; @last-updated: 2019-03-25T18:31Z
  ; @application: SAMI
  ; @version: 18.0
  ; @patch-list: none yet
@@ -23,173 +25,173 @@ SAMIUTCR ;ven/lgc - UNIT TEST for SAMICTR ; 3/15/19 11:15am
  ;
  ; @section 1 code
  ;
-START i $T(^%ut)="" W !,"*** UNIT TEST NOT INSTALLED ***" Q
- d EN^%ut($T(+0),2)
- q
+START if $text(^%ut)="" write !,"*** UNIT TEST NOT INSTALLED ***" quit
+ do EN^%ut($text(+0),2)
+ quit
  ;
  ;
-STARTUP n utsuccess
- n root s root=$$setroot^%wd("vapals-patients")
- k @root@("graph","XXX00001")
- n SAMIUPOO D PLUTARR^SAMIUTST(.SAMIUPOO,"all XXX00001 forms")
- m @root@("graph","XXX00001")=SAMIUPOO
- K SAMIUPOO D PLUTARR^SAMIUTST(.SAMIUPOO,"ceform-2019-01-24")
- m @root@("graph","XXX00001","ceform-2019-01-24")=SAMIUPOO
- K SAMIUPOO D PLUTARR^SAMIUTST(.SAMIUPOO,"ceform-2018-01-02")
- m @root@("graph","XXX00001","ceform-2018-01-02")=SAMIUPOO
- Q
+STARTUP new utsuccess
+ new root set root=$$setroot^%wd("vapals-patients")
+ kill @root@("graph","XXX00001")
+ new SAMIUPOO do PLUTARR^SAMIUTST(.SAMIUPOO,"all XXX00001 forms")
+ merge @root@("graph","XXX00001")=SAMIUPOO
+ kill SAMIUPOO do PLUTARR^SAMIUTST(.SAMIUPOO,"ceform-2019-01-24")
+ merge @root@("graph","XXX00001","ceform-2019-01-24")=SAMIUPOO
+ kill SAMIUPOO D PLUTARR^SAMIUTST(.SAMIUPOO,"ceform-2018-01-02")
+ merge @root@("graph","XXX00001","ceform-2018-01-02")=SAMIUPOO
+ quit
  ;
 SHUTDOWN ; ZEXCEPT: utsuccess
- K utsuccess
- Q
+ kill utsuccess
+ quit
  ;
  ;
 UTQUIT ; @TEST - Quit at top of routine
- D ^SAMICTR
- d SUCCEED^%ut
- q
+ do ^SAMICTR
+ do SUCCEED^%ut
+ quit
  ;
 UTWSRPT ; @TEST - web service which returns an html cteval report
  ;WSREPORT(return,SAMIFLTR)
  ;use ceform-2018-10-21
- n root s root=$$setroot^%wd("vapals-patients")
- k SAMIFLTR
- s SAMIFLTR("studyid")="XXX00001"
- s SAMIFLTR("form")="ceform-2018-10-21"
- s utsuccess=1
- d WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
+ new root set root=$$setroot^%wd("vapals-patients")
+ kill SAMIFLTR
+ set SAMIFLTR("studyid")="XXX00001"
+ set SAMIFLTR("form")="ceform-2018-10-21"
+ set utsuccess=1
+ do WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
  ; compare SAMIUPOO with SAMIUPOO from a Pull
- d PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR")
+ do PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR")
  ; now compare
- n pnode,anode s pnode=$na(SAMIUPOO),anode=$na(SAMIUARC)
- f  s pnode=$q(@pnode),anode=$q(@anode) q:pnode=""  d
- . I '(@pnode=@anode) s utsuccess=0
- s:'(anode="") utsuccess=0
- d CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
+ new pnode,anode set pnode=$name(SAMIUPOO),anode=$name(SAMIUARC)
+ for  set pnode=$query(@pnode),anode=$query(@anode) quit:pnode=""  do
+ . if '(@pnode=@anode) set utsuccess=0
+ set:'(anode="") utsuccess=0
+ do CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
  ;
  ;use ceform-2018-12-03
- k SAMIFLTR
- s SAMIFLTR("studyid")="XXX00001"
- s SAMIFLTR("form")="ceform-2018-12-03"
- s utsuccess=1
- d WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
+ kill SAMIFLTR
+ set SAMIFLTR("studyid")="XXX00001"
+ set SAMIFLTR("form")="ceform-2018-12-03"
+ set utsuccess=1
+ do WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
  ; compare SAMIUPOO with SAMIUPOO from a Pull
- d PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX12-3")
+ do PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX12-3")
  ; now compare
- n pnode,anode s pnode=$na(SAMIUPOO),anode=$na(SAMIUARC)
- f  s pnode=$q(@pnode),anode=$q(@anode) q:pnode=""  d
- . I '(@pnode=@anode) s utsuccess=0
- s:'(anode="") utsuccess=0
- d CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
+ new pnode,anode set pnode=$name(SAMIUPOO),anode=$name(SAMIUARC)
+ for  set pnode=$query(@pnode),anode=$query(@anode) quit:pnode=""  do
+ . if '(@pnode=@anode) set utsuccess=0
+ set:'(anode="") utsuccess=0
+ do CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
  ;
  ;use ceform-2019-01-23
- k SAMIFLTR
- s SAMIFLTR("studyid")="XXX00001"
- s SAMIFLTR("form")="ceform-2019-01-23"
- s ^%wd(17.040801,23,"graph","XXX00001","ceform-2019-01-23","cetex")="b"
- s utsuccess=1
- d WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
+ kill SAMIFLTR
+ set SAMIFLTR("studyid")="XXX00001"
+ set SAMIFLTR("form")="ceform-2019-01-23"
+ set ^%wd(17.040801,23,"graph","XXX00001","ceform-2019-01-23","cetex")="b"
+ set utsuccess=1
+ do WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
  ; compare SAMIUPOO with SAMIUPOO from a Pull
- d PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX01-23")
+ do PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX01-23")
  ; now compare
- n pnode,anode s pnode=$na(SAMIUPOO),anode=$na(SAMIUARC)
- f  s pnode=$q(@pnode),anode=$q(@anode) q:pnode=""  d
- . I '(@pnode=@anode) s utsuccess=0
- s:'(anode="") utsuccess=0
- d CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
+ new pnode,anode set pnode=$name(SAMIUPOO),anode=$name(SAMIUARC)
+ for  set pnode=$query(@pnode),anode=$query(@anode) quit:pnode=""  do
+ . if '(@pnode=@anode) set utsuccess=0
+ set:'(anode="") utsuccess=0
+ do CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
  ;
  ;use ceform-2019-01-24
- k SAMIFLTR
- s SAMIFLTR("studyid")="XXX00001"
- s SAMIFLTR("form")="ceform-2019-01-24"
- s utsuccess=1
- d WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
+ kill SAMIFLTR
+ set SAMIFLTR("studyid")="XXX00001"
+ set SAMIFLTR("form")="ceform-2019-01-24"
+ set utsuccess=1
+ do WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
  ; compare SAMIUPOO with SAMIUPOO from a Pull
- d PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX01-24")
+ do PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX01-24")
  ; now compare
- n pnode,anode s pnode=$na(SAMIUPOO),anode=$na(SAMIUARC)
- f  s pnode=$q(@pnode),anode=$q(@anode) q:pnode=""  d
- . I '(@pnode=@anode) s utsuccess=0
- s:'(anode="") utsuccess=0
- d CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
+ new pnode,anode set pnode=$name(SAMIUPOO),anode=$name(SAMIUARC)
+ for  set pnode=$query(@pnode),anode=$query(@anode) quit:pnode=""  do
+ . if '(@pnode=@anode) set utsuccess=0
+ set:'(anode="") utsuccess=0
+ do CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
  ;
  ;use ceform-2018-01-02
- k SAMIFLTR
- s SAMIFLTR("studyid")="XXX00001"
- s SAMIFLTR("form")="ceform-2018-01-02"
- s utsuccess=1
- d WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
+ kill SAMIFLTR
+ set SAMIFLTR("studyid")="XXX00001"
+ set SAMIFLTR("form")="ceform-2018-01-02"
+ set utsuccess=1
+ do WSREPORT^SAMICTR(.SAMIUPOO,.SAMIFLTR)
  ; compare SAMIUPOO with SAMIUPOO from a Pull
- d PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX01-02")
+ do PLUTARR^SAMIUTST(.SAMIUARC,"UTWSRPT^SAMIUTCR XXX01-02")
  ; now compare
- n pnode,anode s pnode=$na(SAMIUPOO),anode=$na(SAMIUARC)
- f  s pnode=$q(@pnode),anode=$q(@anode) q:pnode=""  d
- . I '(@pnode=@anode) s utsuccess=0
- s:'(anode="") utsuccess=0
- d CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
+ new pnode,anode set pnode=$name(SAMIUPOO),anode=$name(SAMIUARC)
+ for  set pnode=$query(@pnode),anode=$query(@anode) quit:pnode=""  do
+ . if '(@pnode=@anode) set utsuccess=0
+ set:'(anode="") utsuccess=0
+ do CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval FAILED!")
  ;
  ; Now lets try a special case
- n SAMIUPOO,SAMIUARC,pnode,anode
- d PLUTARR^SAMIUTST(.SAMIUPOO,"ce-sb-si forms XXX0005")
- k @root@("graph","XXX0005")
- m @root@("graph","XXX0005")=SAMIUPOO
- k SAMIFLTR
- s SAMIFLTR("studyid")="XXX0005"
- s SAMIFLTR("form")="ceform-2016-01-01"
- s utsuccess=1
- d WSREPORT^SAMICTR(.SAMIUARC,.SAMIFLTR)
- d PLUTARR^SAMIUTST(.SAMIUPOO,"UTLOADD^SAMIUTF2 XXX0005")
+ new SAMIUPOO,SAMIUARC,pnode,anode
+ do PLUTARR^SAMIUTST(.SAMIUPOO,"ce-sb-si forms XXX0005")
+ kill @root@("graph","XXX0005")
+ merge @root@("graph","XXX0005")=SAMIUPOO
+ kill SAMIFLTR
+ set SAMIFLTR("studyid")="XXX0005"
+ set SAMIFLTR("form")="ceform-2016-01-01"
+ set utsuccess=1
+ do WSREPORT^SAMICTR(.SAMIUARC,.SAMIFLTR)
+ do PLUTARR^SAMIUTST(.SAMIUPOO,"UTLOADD^SAMIUTF2 XXX0005")
  ; now compare
- n pnode,anode s pnode=$na(SAMIUPOO),anode=$na(SAMIUARC)
- f  s pnode=$q(@pnode),anode=$q(@anode) q:pnode=""  d
- . I '(@pnode=@anode) s utsuccess=0
- s:'(anode="") utsuccess=0
- k @root@("graph","XXX0005")
- d CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval XXX0005 FAILED!")
- q
+ new pnode,anode set pnode=$name(SAMIUPOO),anode=$name(SAMIUARC)
+ for  set pnode=$query(@pnode),anode=$query(@anode) quit:pnode=""  do
+ . if '(@pnode=@anode) set utsuccess=0
+ set:'(anode="") utsuccess=0
+ kill @root@("graph","XXX0005")
+ do CHKEQ^%ut(utsuccess,1,"Testing web service returns html cteval XXX0005 FAILED!")
+ quit
  ;
 UTOUT ; @TEST - out line
  ;OUT(ln)
- n cnt,rtn,SAMIUPOO
- s cnt=1,rtn="SAMIUPOO",SAMIUPOO(1)="First line of test"
- n SAMIULN s SAMIULN="Second line test"
- s utsuccess=0
- d OUT^SAMICTR(SAMIULN)
- s utsuccess=($g(SAMIUPOO(2))="Second line test")
- d CHKEQ^%ut(utsuccess,1,"Testing out(ln) adds line to array FAILED!")
- q
+ new cnt,rtn,SAMIUPOO
+ set cnt=1,rtn="SAMIUPOO",SAMIUPOO(1)="First line of test"
+ new SAMIULN set SAMIULN="Second line test"
+ set utsuccess=0
+ do OUT^SAMICTR(SAMIULN)
+ set utsuccess=($get(SAMIUPOO(2))="Second line test")
+ do CHKEQ^%ut(utsuccess,1,"Testing out(ln) adds line to array FAILED!")
+ quit
  ;
 UTHOUT ; @TEST - hout line
  ;HOUT(ln)
- n cnt,rtn,SAMIUPOO
- s cnt=1,rtn="SAMIUPOO",SAMIUPOO(1)="First line of test"
- n SAMIULN s SAMIULN="Second line test"
- s utsuccess=0
- d HOUT^SAMICTR(SAMIULN)
- s utsuccess=($g(SAMIUPOO(2))="<p><span class='sectionhead'>Second line test</span>")
- d CHKEQ^%ut(utsuccess,1,"Testing out(ln) adds line to array FAILED!")
- q
+ new cnt,rtn,SAMIUPOO
+ set cnt=1,rtn="SAMIUPOO",SAMIUPOO(1)="First line of test"
+ new SAMIULN set SAMIULN="Second line test"
+ set utsuccess=0
+ do HOUT^SAMICTR(SAMIULN)
+ set utsuccess=($get(SAMIUPOO(2))="<p><span class='sectionhead'>Second line test</span>")
+ do CHKEQ^%ut(utsuccess,1,"Testing out(ln) adds line to array FAILED!")
+ quit
  ;
 UTXVAL ; @TEST - extrinsic which returns the dictionary value defined by var
  ;XVAL(var,vals,dict,valdx)
- s utsuccess=0
- s SAMIUARC(1)="Testing xval"
- s utsuccess=($$XVAL^SAMICTR(1,"SAMIUARC")="Testing xval")
- d CHKEQ^%ut(utsuccess,1,"Testing xval(var,vals) FAILED!")
- q
+ set utsuccess=0
+ set SAMIUARC(1)="Testing xval"
+ set utsuccess=($$XVAL^SAMICTR(1,"SAMIUARC")="Testing xval")
+ do CHKEQ^%ut(utsuccess,1,"Testing xval(var,vals) FAILED!")
+ quit
  ;
 UTXSUB ; @TEST - extrinsic which returns the dictionary value defined by var
  ;XSUB(var,vals,dict,valdx)
- n SAMIUVALS,SAMIUVAR,SAMIUPOO,SAMIUVALDX,SAMIUDICT,result
- s utsuccess=0
- s SAMIUVALS="SAMIUPOO"
- s SAMIUVAR="cteval-dict"
- s SAMIUPOO(1)="biopsy"
- s SAMIUVALDX=1
- s SAMIUDICT=$$setroot^%wd("cteval-dict")
- s result=$$XSUB^SAMICTR(SAMIUVAR,SAMIUVALS,SAMIUDICT,SAMIUVALDX)
- s utsuccess=(result="CT-guided biopsy")
- D CHKEQ^%ut(utsuccess,1,"Testing xsub(var,vals,dict,valdx) FAILED!")
- q
+ new SAMIUVALS,SAMIUVAR,SAMIUPOO,SAMIUVALDX,SAMIUDICT,result
+ set utsuccess=0
+ set SAMIUVALS="SAMIUPOO"
+ set SAMIUVAR="cteval-dict"
+ set SAMIUPOO(1)="biopsy"
+ set SAMIUVALDX=1
+ set SAMIUDICT=$$setroot^%wd("cteval-dict")
+ set result=$$XSUB^SAMICTR(SAMIUVAR,SAMIUVALS,SAMIUDICT,SAMIUVALDX)
+ set utsuccess=(result="CT-guided biopsy")
+ do CHKEQ^%ut(utsuccess,1,"Testing xsub(var,vals,dict,valdx) FAILED!")
+ quit
  ;
 EOR ;End of routine SAMIUTCR

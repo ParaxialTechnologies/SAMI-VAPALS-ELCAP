6339db6d55abca15ba54762eaa86f810ce886b13
George Lilly <glilly@glilly.net>
2019-07-02 08:17:13 -0700

    add a space after thorax

--- routines/SAMICTR0.m
+++ routines/SAMICTR0.m
@@ -148,7 +148,7 @@ WSREPORT(return,filter) ; web service which returns an html cteval report
  i $$XVAL("cectp",vals)="i" d  ;
  . d OUT("Limited Diagnostic CT examination was performed.")
  e  d  ;
- . d OUT("CT examination of the entire thorax was performed at"_$$XSUB("cectp",vals,dict)_" settings.")
+ . d OUT("CT examination of the entire thorax was performed at "_$$XSUB("cectp",vals,dict)_" settings.")
  ;
  i $$XVAL("cectrst",vals)'="" d  ;
  . d OUT(" Images were obtained at "_$$XVAL("cectrst",vals)_" mm slice thickness.")

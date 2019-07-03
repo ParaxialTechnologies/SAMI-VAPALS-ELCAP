b3d58e05097ea96b71af19291a5f2f040928ef94
George Lilly <glilly@glilly.net>
2019-07-03 13:19:51 -0700

    added a space for prior scan

--- routines/SAMICTR0.m
+++ routines/SAMICTR0.m
@@ -140,7 +140,7 @@ WSREPORT(return,filter) ; web service which returns an html cteval report
  . d OUT("Comparisons: "_$$XVAL("cedcs",vals))
  . s nopri=0
  if $$XVAL("cedps",vals)'="" d  ;
- . d OUT($$XVAL("cedps",vals))
+ . d OUT(" "_$$XVAL("cedps",vals))
  . s nopri=0
  d:nopri OUT("None")
  ;
@@ -148,7 +148,7 @@ WSREPORT(return,filter) ; web service which returns an html cteval report
  i $$XVAL("cectp",vals)="i" d  ;
  . d OUT("Limited Diagnostic CT examination was performed.")
  e  d  ;
- . d OUT("CT examination of the entire thorax was performed at"_$$XSUB("cectp",vals,dict)_" settings.")
+ . d OUT("CT examination of the entire thorax was performed at "_$$XSUB("cectp",vals,dict)_" settings.")
  ;
  i $$XVAL("cectrst",vals)'="" d  ;
  . d OUT(" Images were obtained at "_$$XVAL("cectrst",vals)_" mm slice thickness.")
@@ -158,7 +158,7 @@ WSREPORT(return,filter) ; web service which returns an html cteval report
  . n nvadbo s nvadbo=1
  . n ii
  . f ii="ceoaa","ceaga","ceasa","ceala","ceapa","ceaaa","ceaka" d  ;
- . . i $$XVAL(ii,vals)="e" set nvadbo=0
+ . . i $$XVAL(ii,vals)="y" set nvadbo=0
  . ;
  . i nvadbo=1 d  ;
  . . d OUT("Upper abdominal images were not acquired on the current scan due to its limited nature.")

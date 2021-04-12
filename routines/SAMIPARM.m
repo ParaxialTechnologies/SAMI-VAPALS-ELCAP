354b492ccf0d0c023d98a2d2331dbcb7c65dccd9
George Lilly <glilly@glilly.net>
2021-04-12 14:12:53 -0700

    test for params web service

---
+++ routines/SAMIPARM.m
@@ -0,0 +1,21 @@
+SAMIPARM ;ven/gpl - vapals/elcap parameter service ;2021-03-21T23:51Z
+ ;;18.0;SAMI;**10**;2020-01;Build 11
+ ;
+ Q
+ ;
+WSPARAMS(parmsjson,filter) ; web service that returns the paramters to use
+ ; for the site, passed in as fliter("site"), as json
+ d  ;
+ . n parms
+ . s parms("ssn")="PID"
+ . s parms("socialSecurityNumber")="Patient ID"
+ . ; and so on
+ . ;n parmsjson
+ . d ENCODE^%webjson("parms","parmsjson")
+ . set HTTPRSP("mime")="application/json"
+ q
+ ;
+ADDSVC() ; add the params webservice to the system
+ d addService^%webutils("GET","params","WSPARAMS^SAMIPARM")
+ Q
+ ;

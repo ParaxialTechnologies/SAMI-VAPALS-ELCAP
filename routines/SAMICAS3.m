6edc06109a8fc7c223feee61f3a4675bfcc44fe7
George Lilly <glilly@glilly.net>
2021-05-26 12:20:35 -0700

    copy forward for Intervention form

--- routines/SAMICAS3.m
+++ routines/SAMICAS3.m
@@ -1,5 +1,5 @@
 SAMICAS3 ;ven/gpl - ielcap: case review page (cont) ;2021-05-25T14:52Z
- ;;18.0;SAMI;**3,9,11**;2020-01;Build 11
+ ;;18.0;SAMI;**3,9,11**;2020-01;Build 21
  ;;1.18.0.11+i11
  ;
  ; SAMICAS3 contains ppis and other subroutines to support processing
@@ -715,6 +715,15 @@ MKITFORM(sid,key) ; create intervention form
  quit:+sien=0
  ;
  new cdate set cdate=$piece(key,"itform-",2)
+ ; nodule copy
+ ;n srckey s srckey=$$PREVNOD(sid)
+ n srckey,srcdate s srcdate=$$LASTCMP(sid,.srckey)
+ i srckey'="" d  ;
+ . new target,source
+ . set source=$name(@root@("graph",sid,srckey))
+ . set target=$name(@root@("graph",sid,key))
+ . d CTCOPY^SAMICTC1(source,target,key)
+ ; end nodule copy
  merge @root@("graph",sid,key)=@root@(sien)
  set @root@("graph",sid,key,"samicreatedate")=cdate
  do SSAMISTA^SAMICASE(sid,key,"incomplete")

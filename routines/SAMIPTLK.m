c20b9b20f39358877df9372299ee2d2b4f4a6ba3
Frederick D. S. Marshall <rick.marshall@vistaexpertise.net>
2024-09-09 20:00:27 -0700

    annotate, add logs, version-control lines, hdrs, bump dates

--- routines/SAMIPTLK.m
+++ routines/SAMIPTLK.m
@@ -1,21 +1,171 @@
-SAMIPTLK ;ven/gpl - SAMI patient lookup routines ;Dec 17, 2019@09:44
- ;;18.0;SAMI;;;Build 11
+SAMIPTLK ;ven/gpl - patient lookup; 2024-09-10t02:12z
+ ;;18.0;SAMI;**5,7,18**;2020-01-17;
+ ;mdc-e1;SAMIPTLK-20240910-E3bjqxe;SAMI-18-18-b1
+ ;mdc-v7;B50934240;SAMI*18.0*18 SEQ #18
  ;
- ;@license: see routine SAMIUL
+ ; SAMIPTLK contains web services and subroutines to support the
+ ; lookup of patients in ScreeningPlus.
  ;
- ; Authored by George P. Lilly 2018
+ quit  ; no entry from top
  ;
- Q
-WSPTLOOK(rtn,filter) ; patient lookup - calls HMPPTRPC
+ ;
+ ;
+ ;
+ ;@section 0 primary development
+ ;
+ ;
+ ;
+ ;
+ ;@routine-credits
+ ;
+ ;@dev George P. Lilly (gpl)
+ ; gpl@vistaexpertise.net
+ ;@dev-org Vista Expertise Network (ven)
+ ; http://vistaexpertise.net
+ ;@copyright 2018/2024, gpl, all rights reserved
+ ;@license see routine SAMIUL
+ ;
+ ;@update 2024-09-10t02:12z
+ ;@app-suite Screening Applications Management - SAM
+ ;@app ScreeningPlus (SAM-IELCAP) - SAMI
+ ;@suite-of-files SAMI Forms (311.101-311.199)
+ ;@release 18-18
+ ;@edition-date 2020-01-17
+ ;@patches **5,7,18**
+ ;
+ ;@dev-add Frederick D. S. Marshall (toad)
+ ; toad@vistaexpertise.net
+ ;@dev-add Kenneth W. McGlothlen (mcglk)
+ ; mcglk@vistaexpertise.net
+ ;@dev-add Linda M. R. Yaw (lmry)
+ ; linda.yaw@vistaexpertise.net
+ ;@dev-add Larry G. Carlson (lgc)
+ ; larry.g.carlson@gmail.com
+ ;@dev-add Alexis R. Carlson (arc)
+ ; whatisthehumanspirit@gmail.com
+ ;
+ ;@module-credits
+ ;
+ ;@project VA Partnership to Increase Access to Lung Screening
+ ; (VA-PALS)
+ ; http://va-pals.org/
+ ;@funding 2017/2021, Bristol-Myers Squibb Foundation (bmsf)
+ ; https://www.bms.com/about-us/responsibility/bristol-myers-squibb-foundation.html
+ ;@partner-org Veterans Affairs Office of Rural health
+ ; https://www.ruralhealth.va.gov/
+ ;@partner-org International Early Lung Cancer Action Program (I-ELCAP)
+ ; http://ielcap.com/
+ ;@partner-org Paraxial Technologies (par)
+ ; http://paraxialtech.com/
+ ;@partner-org Open Source Electronic Health Record Alliance (OSEHRA)
+ ; https://www.osehra.org/groups/va-pals-open-source-project-group
+ ;
+ ;@project I-ELCAP AIRS Automated Image Reading System
+ ; https://www.ielcap-airs.org
+ ;@funding 2024, Mt. Sinai Hospital (msh)
+ ;@partner-org par
+ ;
+ ;@module-log repo github.com:VA-PALS-ELCAP/SAMI-VAPALS-ELCAP.git
+ ;
+ ; 2020-01-16 ven/lgc 18-0 0a2af965
+ ;  SAMIPTLK (F1Y8RNE B9242590 EJomiC)
+ ; Bulk commit due to switch to Cache.
+ ;
+ ; 2020-04-11 ven/gpl 18-5 2f2c29c1
+ ;  SAMIPTLK (F1jn7RT B10261104 EVRktE)
+ ; the crux of multi-tenancy.
+ ;
+ ; 2020-04-11 ven/gpl 18-5 d895cd7a
+ ;  SAMIPTLK (FCOM8k B10220412 E2+2jaV)
+ ; turn off debugging call.
+ ;
+ ; 2020-08-24 ven/gpl 18-7 135e8fdf
+ ;  SAMIPTLK (F1%FgrV B11168340 E3CdCfT)
+ ; utility to change patient site.
+ ;
+ ; 2024-09-07 ven/gpl 18-18-b1 e6fdd47d
+ ;  SAMIPTLK (F1Cgyfq B15734046 E2Q2QC2)
+ ; mrn lookup + display working.
+ ;
+ ; 2024-09-10 ven/toad 18-18-b1
+ ;  SAMIPTLK (F??? B50934240 E???)
+ ; add hdr + subrtn hdr comments, log, bump version + dates.
+ ;
+ ;@to-do
+ ;
+ ; fill in log before 18-18, add details
+ ;
+ ;@contents
+ ;
+ ; WSPTLOOK patient lookup
+ ; WSPTLKUP patient lookup from patient-lookup cache
+ ; BUILDRTN build patient-lookup return value
+ ;
+ ;
+ ;
+ ;
+ ;@section 1 subroutines
+ ;
+ ;
+ ;
+ ;
+ ;@ws-code WSPTLOOK
+ ;
+ ;@stanza 1 invocation, binding, & branching
+ ;
+ ;ven/gpl;ws;procedure;silent;clean?;sac?;tests?;port?
+ ;@called-by [tbd]
+ ;@calls
+ ; SELECT^HMPPTRPC
+ ; ENCODE^VPRJSON
+ ;@input
+ ; filter
+ ;@output
+ ; .rtn
+ ;@examples [tbd]
+ ;@tests [tbd]
+ ;
+ ;
+WSPTLOOK(rtn,filter) ; patient lookup
+ ;
+ ;@stanza 2 lookup
  ;
  n search s search=$g(filter("search"))
  n rslt
  d SELECT^HMPPTRPC(.rslt,"NAME",search)
  i $d(rslt) d  ;
- . D ENCODE^VPRJSON("rslt","rtn")
- q
+ . d ENCODE^VPRJSON("rslt","rtn")
+ . q
+ ;
+ ;
+ ;@stanza 3 termination
+ ;
+ quit  ; end of ws WSPTLOOK
+ ;
+ ;
+ ;
+ ;
+ ;@ws-code WSPTLKUP
+ ;
+ ;@stanza 1 invocation, binding, & branching
+ ;
+ ;ven/gpl;ws;procedure;silent;clean?;sac?;tests?;port?
+ ;@called-by [tbd]
+ ;@calls
+ ; $$setroot^%wd
+ ; $$UPCASE^XLFMSMT
+ ; BUILDRTN
+ ;@input
+ ; filter
+ ;@output
+ ; .rtn
+ ;@examples [tbd]
+ ;@tests [tbd]
+ ;
  ;
 WSPTLKUP(rtn,filter) ; patient lookup from patient-lookup cache
+ ;
+ ;@stanza 2 setup
  ;
  n root s root=$$setroot^%wd("patient-lookup")
  n search s search=$g(filter("search"))
@@ -31,6 +181,10 @@ WSPTLKUP(rtn,filter) ; patient lookup from patient-lookup cache
  n p1,p2
  s p1=$p(search,",",1)
  s p2=$p(search,",",2)
+ ;
+ ;
+ ;@stanza 3 try bs5 lookup
+ ;
  i $l(search)=5 i +$e(search,2,5)>0 d  ;q  ; using last5
  . n gn2 s gn2=$na(@root@("last5"))
  . n ii s ii=""
@@ -38,9 +192,15 @@ WSPTLKUP(rtn,filter) ; patient lookup from patient-lookup cache
  . . i $g(@root@(ii,"siteid"))'=site q
  . . s cnt=cnt+1
  . . s rslt(cnt,ii)=""
+ . . q
  . i cnt>0 d  ;
  . . d BUILDRTN(.rtn,.rslt,$g(filter("format")))
- ; 
+ . . q
+ . q
+ ;
+ ;
+ ;@stanza 4 try partial-match lookup
+ ;
  n have s have=""
  n q1 s q1=$na(@gn@(p1))
  n q1x s q1x=$e(q1,1,$l(q1)-1) ; removes the )
@@ -50,6 +210,7 @@ WSPTLKUP(rtn,filter) ; patient lookup from patient-lookup cache
  . n exit s exit=0
  . i p2'="" d  ;
  . . i p2'=$e($p(qx,",",5),1,$l(p2)) s exit=1
+ . . q
  . q:exit
  . n qx2 s qx2=+$p(qx,",",6)
  . i $g(@root@(qx2,"siteid"))'[site q  ;
@@ -58,6 +219,11 @@ WSPTLKUP(rtn,filter) ; patient lookup from patient-lookup cache
  . s have(qx2)=""
  . s rslt(cnt,qx2)="" ; the ien
  . ;w !,qx," ien=",$o(rslt(cnt,""))
+ . q
+ ;
+ ;
+ ;@stanza 5 try emrn lookup
+ ;
  s gn3=$na(@root@("emrn"))
  s q1="e"_p1
  ;
@@ -73,18 +239,58 @@ WSPTLKUP(rtn,filter) ; patient lookup from patient-lookup cache
  . s have(lien)=""
  . s rslt(cnt,lien)="" ; the ien
  . ;w !,q1," ien=",$o(rslt(cnt,""))
+ . q
+ ;
+ ;
+ ;@stanza 6 build results return value
  ;
  i cnt>0 d BUILDRTN(.rtn,.rslt,$g(filter("format")))
- q
  ;
-BUILDRTN(rtn,ary,format) ; build the return json unless format=array
- ; then return a mumps array
+ ;
+ ;@stanza 7 termination
+ ;
+ quit  ; end of ws WSPTLKUP
+ ;
+ ;
+ ;
+ ;
+ ;@proc BUILDRTN
+ ;
+ ;@stanza 1 invocation, binding, & branching
+ ;
+ ;ven/gpl;private;procedure;silent;clean?;sac?;tests?;port?
+ ;@called-by
+ ; WSPTLKUP
+ ;@calls
+ ; ^ZTER (commented out)
+ ; $$setroot^%wd
+ ; $$GETUID^SAMIUID
+ ; ENCODE^VPRJSON
+ ;@input
+ ; ary
+ ; format
+ ;@output
+ ; .rtn
+ ;@examples [tbd]
+ ;@tests [tbd]
+ ;
+ ; build return json unless format=array, then return a mumps array
+ ;
+ ;
+BUILDRTN(rtn,ary,format) ; build patient-lookup return value
+ ;
+ ;
+ ;@stanza 2 setup
  ;
  ;d ^ZTER
  n root s root=$$setroot^%wd("patient-lookup")
  n groot s groot=$$setroot^%wd("vapals-patients")
  n zi s zi=""
  n r1 s r1=""
+ ;
+ ;
+ ;@stanza 3 main loop
+ ;
  f  s zi=$o(ary(zi)) q:zi=""  d  ;
  . n rx s rx=$o(ary(zi,""))
  . s r1("result",zi,"name")=$g(@root@(rx,"saminame"))
@@ -99,6 +305,9 @@ BUILDRTN(rtn,ary,format) ; build the return json unless format=array
  . i $o(@groot@("dfn",dfn,""))'="" d  ;
  . . s r1("result",zi,"vapals")=1
  . . s r1("result",zi,"studyid")=$g(@groot@(dfn,"samistudyid"))
+ . . q
+ . q
+ ;
  ;.;
  ;.; ven/lgc 2019-12-17 missing forms
  ;.;
@@ -106,7 +315,18 @@ BUILDRTN(rtn,ary,format) ; build the return json unless format=array
  ;. . s r1("result",zi,"vapals")=0
  ;
  ;q:'$d(r1)
+ ;
+ ;
+ ;@stanza 4 build result structure
+ ;
  i format="array" m rtn=r1 q  ; return a mumps array
  d ENCODE^VPRJSON("r1","rtn")
- q
  ;
+ ;
+ ;@stanza 5 termination
+ ;
+ quit  ; end of BUILDRTN
+ ;
+ ;
+ ;
+EOR ; end of routine SAMIPTLK
--- routines/SAMIUID.m
+++ routines/SAMIUID.m
@@ -1,31 +1,47 @@
-SAMIDCM2 ;ven/gpl - SCREEN+ USER ID ROUTINES ; 2022-01-18t00:42z
- ;;18.0;SAMI;**16**;2020-01;Build 2
- ;18-16
+SAMIUID ;ven/gpl - user id; 2024-09-10t02:37z
+ ;;18.0;SAMI;**18**;2020-01-17;
+ ;mdc-e1;SAMIUID-20240910-E2WaTc0;SAMI-18-18-b1
+ ;mdc-v7;B15836538;SAMI*18.0*18 SEQ #18
  ;
- ;@license: see routine SAMIUL
+ ; SAMIUID contains private program services to identify the current
+ ; ScreeningPlus user.
+ ;
+ quit  ; no entry from top
+ ;
+ ;
+ ;
  ;
  ;@section 0 primary development
  ;
  ;
  ;
+ ;
  ;@routine-credits
- ;@dev-main George P. Lilly (gpl)
+ ;
+ ;@dev George P. Lilly (gpl)
  ; gpl@vistaexpertise.net
- ;@dev-org-main Vista Expertise Network (ven)
+ ;@dev-org Vista Expertise Network (ven)
  ; http://vistaexpertise.net
- ;@copyright 2017/2021, gpl, all rights reserved
+ ;@copyright 2017/2024, gpl, all rights reserved
  ;@license see routine SAMIUL
  ;
- ;@last-update 2022-01-18t00:42z
- ;@application Screening Applications Management (SAM)
- ;@module Screening Applications Management - IELCAP (SAMI)
+ ;@update 2024-09-10t02:37z
+ ;@app-suite Screening Applications Management - SAM
+ ;@app ScreeningPlus (SAM-IELCAP) - SAMI
  ;@suite-of-files SAMI Forms (311.101-311.199)
- ;@version 18-16
- ;@release-date 2020-01
- ;@patch-list **16**
+ ;@release 18-18
+ ;@edition-date 2020-01-17
+ ;@patches **18**
  ;
+ ;@dev-add Frederick D. S. Marshall (toad)
+ ; toad@vistaexpertise.net
+ ;@dev-add Kenneth W. McGlothlen (mcglk)
+ ; mcglk@vistaexpertise.net
+ ;@dev-add Linda M. R. Yaw (lmry)
+ ; linda.yaw@vistaexpertise.net
  ;
  ;@module-credits
+ ;
  ;@project VA Partnership to Increase Access to Lung Screening
  ; (VA-PALS)
  ; http://va-pals.org/
@@ -40,12 +56,52 @@ SAMIDCM2 ;ven/gpl - SCREEN+ USER ID ROUTINES ; 2022-01-18t00:42z
  ;@partner-org Open Source Electronic Health Record Alliance (OSEHRA)
  ; https://www.osehra.org/groups/va-pals-open-source-project-group
  ;
+ ;@project I-ELCAP AIRS Automated Image Reading System
+ ; https://www.ielcap-airs.org
+ ;@funding 2024, Mt. Sinai Hospital (msh)
+ ;@partner-org par
+ ;
  ;@module-log repo github.com:VA-PALS-ELCAP/SAMI-VAPALS-ELCAP.git
  ;
+ ; 2024-09-07 ven/gpl 18-18-b1 e6fdd47d
+ ;  SAMIUID (F2LNW6y B9193785 E31giZd)
+ ; add rtn w/GETUID,GETIDSID: mrn lookup + display working.
+ ;
+ ; 2024-09-10 ven/toad 18-18-b1
+ ;  SAMIUID (F??? B15836538 E???)
+ ; add hdr + subrtn hdr comments, log, bump version + dates.
+ ;
+ ;@to-do
+ ;
+ ; fill in log before 18-18, add details
+ ;
+ ;@contents
+ ;
+ ; $$GETUID = id to use
+ ; $$GETIDSID = id to use based on sid
  ;
  ;
  ;
- Q
+ ;
+ ;@section 1 subroutines
+ ;
+ ;
+ ;
+ ;
+ ;@function $$GETUID
+ ;
+ ;@stanza 1 invocation, binding, & branching
+ ;
+ ;ven/gpl;private;function;silent;clean?;sac?;tests?;port?
+ ;@called-by
+ ; $$GETIDSID^SAMIUID
+ ;@calls
+ ; $$setroot^%wd
+ ;@input
+ ; dfn
+ ;@output = uid
+ ;@examples [tbd]
+ ;@tests [tbd]
  ;
  ; The hierarchy of user IDs
  ; 1. pid, if available. this is the manually assigned "Study ID"
@@ -65,7 +121,8 @@ SAMIDCM2 ;ven/gpl - SCREEN+ USER ID ROUTINES ; 2022-01-18t00:42z
  ;    is thus the only identifier which is guarentied to exist for every
  ;    person
  ;
-GETUID(dfn) ; extrinsic return the id to use 
+ ;
+GETUID(dfn) ; extrinsic return the id to use
  ;
  n id s id=""
  n lroot s lroot=$$setroot^%wd("patient-lookup")
@@ -75,6 +132,7 @@ GETUID(dfn) ; extrinsic return the id to use
  n lien s lien=$o(@lroot@("dfn",dfn,""))
  w:debug !,"lien= ",lien
  q:lien="" ""
+ ;
  n pien s pien=$o(@proot@("dfn",dfn,""))
  w:debug !,"pien= ",pien
  q:pien="" ""
@@ -82,35 +140,66 @@ GETUID(dfn) ; extrinsic return the id to use
  s id=$g(@proot@(pien,"sipid"))
  if id="" s id=$g(@proot@(pien,"pid"))
  w:debug !,"pid= ",id
- q:id'="" id
+ q:id]"" id
+ ;
  s id=$g(@proot@(pien,"simrn"))
  i id="" s id=$g(@proot@(pien,"mrn"))
  w:debug !,"mrn= ",id
- q:id'="" id
+ q:id]"" id
+ ;
  s id=$g(@lroot@(lien,"sissn"))
  i id="" s id=$g(@lroot@(lien,"ssn"))
  i id="" s id=$g(@proot@(pien,"sissn"))
  w:debug !,"ssn= ",id
- q:id'="" id
+ q:id]"" id
+ ;
  s id=$g(@lroot@(lien,"last5"))
  w:debug !,"last5= ",id
- q:id'="" id
+ q:id]"" id
+ ;
  s id=$g(@proot@(pien,"sid"))
  i id="" s id=$g(@proot@(pien,"sisid"))
  w:debug !,"sid= ",id
- q:id'="" id
+ q:id]"" id
+ ;
  s id=dfn
  w:debug !,"dfn= ",id
- q id
  ;
-GETIDSID(sid) ; extrinsic returns id to use based on sid
+ quit id ; end of $$GETUID
+ ;
+ ;
+ ;
+ ;
+ ;@pps $$GETIDSID^SAMIUID
+ ;
+ ;@stanza 1 invocation, binding, & branching
+ ;
+ ;ven/gpl;pps;function;silent;clean?;sac?;tests?;port?
+ ;@called-by [tbd]
+ ;@calls
+ ; $$setroot^%wd
+ ; $$GETUID
+ ;@input
+ ; sid
+ ;@output = id to use
+ ;@examples [tbd]
+ ;@tests [tbd]
+ ;
+ ;
+GETIDSID(sid) ; id to use based on sid
+ ;
  n debug s debug=$g(DEBUG)
  n proot s proot=$$setroot^%wd("vapals-patients")
  n pien s pien=$o(@proot@("sid",sid,""))
  w:debug !,"pien= ",pien
  q:pien="" ""
+ ;
  n dfn s dfn=$g(@proot@(pien,"dfn"))
  w:debug !,"dfn= ",dfn
- q:dfn="" ""
- q $$GETUID(dfn)
  ;
+ quit:dfn="" ""
+ quit $$GETUID(dfn) ; end of pps $$GETIDSID^SAMIUID
+ ;
+ ;
+ ;
+EOR ; end of routine SAMIUID

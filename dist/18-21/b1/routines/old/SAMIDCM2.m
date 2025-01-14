0a0fd843a480dd3e8aff22eb4a52d2601dff0897
Linda Yaw <linda.yaw@vistaexpertise.net>
2025-01-14 11:11:16 -1000

    More 18-21 prep

--- dist/18-21/b1/routines/old/SAMIDCM2.m
+++ dist/18-21/b1/routines/old/SAMIDCM2.m
@@ -141,7 +141,8 @@ ADDITEMS(ARY,SID) ; add Image items to Items array
  . ;
  . n siuid s siuid=$g(@root@(ien,"json","StudyInstanceUID"))
  . q:siuid=""
- . ;
+ . n siurl s siurl=$g(@root@(ien,"json","url"))
+ . i siurl'="" s siuid=siurl
  . ; add each image item only once to Items array
  . q:$d(images(siuid))
  . s images(siuid)=""
@@ -238,11 +239,11 @@ VIEWURL(SITE) ; URL to use for image viewer
  ;
  ; for this site
  ;
- n url s url="https://viewer.ohif.org/viewer"
+ n zurl s zurl="https://viewer.ohif.org/viewer"
  ; 
  ; todo look up the url from the parameter file
  ;
- quit url ; end of $$VIEWURL
+ quit zurl ; end of $$VIEWURL
  ;
  ;
  ;
